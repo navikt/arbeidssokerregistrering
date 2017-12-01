3c17cdf8c6de33c92693a18e64d05a35ae9c568f
jcpgithub <joselito.poblete@gmail.com>
2017-12-01 17:09:36 +0100

    FO-30 Bruk nav-frontend og jetty server

--- src/tekster/alle-tekster.ts
+++
@@ -1,1 +0,0 @@
-export default {'nb':{'beskrivelse-registrerdeg':'Her får du oppgaver du må gjøre for å styrke deg selv som jobbsøker. Her ligger også ressurser som kan være nyttige i jobbsøkingen.','overskrift-registrerdeg':'Registrer deg som arbeidssøker'},'en':{}};
