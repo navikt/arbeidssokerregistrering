f22c9a942780217ccf43ab47872b3f48b7759f21
Joselito Poblete <joselito.poblete@gmail.com>
2018-01-02 13:53:47 +0100

    FO-32 Lagt til test dersom url sti ikke finnes - f.eks skjema/finnesIkke

--- src/frontend/src/skjema/skjema.test.tsx
+++ src/frontend/src/skjema/skjema.test.tsx
@@ -17,6 +17,10 @@ enzyme.configure({ adapter: new Adapter()});
 beforeEach(() => store.dispatch(setInitalState()));
 
 describe('<Skjema />', () => {
+
+    /*
+    * Navigering til neste side
+    * */
     it('Neste skal ikke være disabled når spørsmål er besvart', () => {
         store.dispatch(endreSvarAction('1', '1'));
         const props = {
@@ -31,6 +35,7 @@ describe('<Skjema />', () => {
         const knappNeste = wrapper.find(KnappNeste);
         expect(knappNeste.props().disabled).to.be.false;
     });
+
     it('Neste skal være disabled når spørsmål ikke er besvart', () => {
         const props = {
             match: {
@@ -44,35 +49,7 @@ describe('<Skjema />', () => {
         const knappNeste = wrapper.find(KnappNeste);
         expect(knappNeste.props().disabled).to.be.true;
     });
-    it('Fullfør skal ikke være synlig på første side', () => {
-        const props = {
-            match: {
-                params: {
-                    id: '1'
-                }
-            },
-            sporsmalErBesvart: (id) => true
-        };
 
-        const wrapper = shallowwithStoreAndIntl((<Skjema {...props} />)).dive().dive();
-        const knappNeste = wrapper.find(KnappFullfor);
-        expect(knappNeste.length).to.be.equal(0);
-    });
-    it('Fullfør skal være synlig på siste side', () => {
-        const lastId = Object.keys(configSpmPrSide).length.toString();
-        const props = {
-            match: {
-                params: {
-                    id: lastId
-                }
-            },
-            sporsmalErBesvart: (id) => true
-        };
-
-        const wrapper = shallowwithStoreAndIntl((<Skjema {...props} />)).dive().dive();
-        const knappNeste = wrapper.find(KnappFullfor);
-        expect(knappNeste.length).to.be.equal(1);
-    });
     it('Skal navigere til neste side', () => {
         const push = sinon.spy();
         const props = {
@@ -89,6 +66,82 @@ describe('<Skjema />', () => {
 
         const wrapper = shallowwithStoreAndIntl((<Skjema {...props} />)).dive().dive();
         wrapper.find(KnappNeste).simulate('click');
+        expect(push).to.have.property('callCount', 1);
+    });
+
+    /*
+    * Fullfør knapp
+    * */
+    it('Fullfør skal ikke være synlig på første side', () => {
+        const props = {
+            match: {
+                params: {
+                    id: '1'
+                }
+            },
+            sporsmalErBesvart: (id) => true
+        };
+
+        const wrapper = shallowwithStoreAndIntl((<Skjema {...props} />)).dive().dive();
+        const knappNeste = wrapper.find(KnappFullfor);
+        expect(knappNeste.length).to.be.equal(0);
+    });
+
+    it('Fullfør skal være synlig på siste side', () => {
+        const lastId = Object.keys(configSpmPrSide).length.toString();
+        const props = {
+            match: {
+                params: {
+                    id: lastId
+                }
+            },
+            sporsmalErBesvart: (id) => true
+        };
+
+        const wrapper = shallowwithStoreAndIntl((<Skjema {...props} />)).dive().dive();
+        const knappNeste = wrapper.find(KnappFullfor);
+        expect(knappNeste.length).to.be.equal(1);
+    });
+
+    it('Fullfør skal være enablet når alle svarene er besvart', () => {
+        const lastId = Object.keys(configSpmPrSide).length.toString();
+        const props = {
+            match: {
+                params: {
+                    id: lastId
+                }
+            },
+            sporsmalErBesvart: (id) => true
+        };
+
+        store.dispatch(endreSvarAction('1', '1'));
+        store.dispatch(endreSvarAction('2', '1'));
+        store.dispatch(endreSvarAction('3', '1'));
+        store.dispatch(endreSvarAction('4', '1'));
+        store.dispatch(endreSvarAction('5', '1'));
+
+        const wrapper = shallowwithStoreAndIntl((<Skjema {...props} />)).dive().dive();
+        const knappNeste = wrapper.find(KnappFullfor);
+        expect(knappNeste.props().disabled).to.be.false;
+    });
+
+    /*
+    * Navigering til skjema/1 - dersom id ikke finnes - f.eks skjema/finnesIkke
+    * */
+    it('Skal navigere til første side (skjema/1), dersom id ikke finnes (f.eks skjema/finnesIkke)', () => {
+        const push = sinon.spy();
+        const props = {
+            match: {
+                params: {
+                    id: 'finnesIkke'
+                }
+            },
+            history: {
+                push
+            },
+        };
+
+        shallowwithStoreAndIntl((<Skjema {...props} />)).dive().dive();
         expect(push).to.have.property('callCount', 1);
     });
 });
