aff00d8b28571ea2c6f3e21a65e0f23a23de6b5c
Joselito Poblete <joselito.poblete@gmail.com>
2018-01-18 10:18:01 +0100

    FO-110 Fiks test som feiler

--- src/frontend/src/oppfolgingsstatus/oppfolgingsstatus-feilmelding.tsx
+++ src/frontend/src/oppfolgingsstatus/oppfolgingsstatus-feilmelding.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import AlertStripe from 'nav-frontend-alertstriper';
 import { Normaltekst } from 'nav-frontend-typografi';
 import { FormattedMessage } from 'react-intl';
+import IntlProvider from '../Intl-provider';
 
 function OppfolgingsstatusFeilmelding() {
-    return(
-        <AlertStripe type="advarsel">
-            <Normaltekst><FormattedMessage id="feil-i-systemene-beskrivelse"/></Normaltekst>
-        </AlertStripe>
+    return (
+        <IntlProvider>
+            <AlertStripe type="advarsel">
+                <Normaltekst><FormattedMessage id="feil-i-systemene-beskrivelse"/></Normaltekst>
+            </AlertStripe>
+        </IntlProvider>
     );
 }
 
