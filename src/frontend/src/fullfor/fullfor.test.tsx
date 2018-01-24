c7da0db10a3db12f791a9cbebeaf15551b710e28
Joselito Poblete <joselito.poblete@gmail.com>
2018-01-24 13:21:43 +0100

    FO-35 Legger til tester på på knapp og sjekkboks

--- src/frontend/src/fullfor/fullfor.test.tsx
+++ src/frontend/src/fullfor/fullfor.test.tsx
@@ -8,12 +8,19 @@ import { shallowWithIntl } from 'enzyme-react-intl';
 import Fullfor from './fullfor';
 import KnappFullfor from '../skjema/knapp-fullfor';
 import {REGVELLYKKET_PATH} from "../utils/konstanter";
+import {Checkbox} from "nav-frontend-skjema";
 
 enzyme.configure({ adapter: new Adapter()});
 
 describe('<Fullfor />', () => {
-    it('Skal ha fullfor knapp', () => {
+    it('Skal ha fullfor knapp som er inaktiv', () => {
+        const props = {};
 
+        const wrapper = enzyme.shallow((<Fullfor {...props} />)).dive();
+        const knappFullfor = wrapper.find(KnappFullfor);
+        expect(knappFullfor.props().disabled).to.be.true;
+    });
+    it('Skal ha fullfor knapp til riktig url', () => {
         const push = sinon.spy();
         const props = {
             history: {
@@ -22,7 +29,18 @@ describe('<Fullfor />', () => {
         };
 
         const wrapper = enzyme.shallow((<Fullfor {...props} />)).dive();
-        wrapper.find(KnappFullfor).simulate('click');
+        const knappFullfor = wrapper.find(KnappFullfor);
+        knappFullfor.simulate('click');
         expect(push.firstCall.args[0]).to.be.equal(`${REGVELLYKKET_PATH}`);
+    });
+    it('Skal sette markert til true når sjekkboks markeres', () => {
+        const props = {};
+
+        const wrapper = enzyme.shallow((<Fullfor {...props} />));
+        const sjekkboks = wrapper.dive().find(Checkbox).dive();
+        const input = sjekkboks.find('input[type="checkbox"]');
+        input.simulate('change');
+
+        expect(wrapper.state().markert).to.be.true;
     });
 });
