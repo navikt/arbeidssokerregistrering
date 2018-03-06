01fc1b8679d7df4a589430d18fedb10355b178e3
Christian Finstad <christian.finstad@nav.no>
2018-03-06 15:43:27 +0100

    FO-588 Endringer etter fagfellevurdering

--- src/frontend/src/oppsummering/uenig-selvgaende.tsx
+++ src/frontend/src/oppsummering/uenig-selvgaende.tsx
@@ -5,18 +5,18 @@ import { Element, Normaltekst, Sidetittel } from 'nav-frontend-typografi';
 import { FormattedMessage } from 'react-intl';
 import { Knapp } from 'nav-frontend-knapper';
 import Knapperad from '../komponenter/knapper/knapperad';
-import { SBLREG_PATH } from '../utils/konstanter';
-import { DITTNAV_URL } from '../ducks/api';
+import { AVBRYT_PATH, SBLREG_PATH } from '../utils/konstanter';
+import { Link } from 'react-router-dom';
 
 function UenigSelvgaende({history}: RouteComponentProps<MatchProps>) {
     return(
-        <div className="panel-blokk">
-            <Sidetittel className="blokk-xl text-align-center">
+        <div className="panel-blokk wrapper-uenig-selvgaende">
+            <Sidetittel className="blokk-l text-align-center">
                 <FormattedMessage id="uenig-selvgaende.tittel"/>
             </Sidetittel>
             <Normaltekst className="blokk-xl"><FormattedMessage id="uenig-selvgaende.innhold"/></Normaltekst>
             <Knapperad>
-                <Knapp className="mmr blokk-s" type="standard" onClick={() => history.goBack()}>
+                <Knapp className="knapp-angre blokk-s" type="standard" onClick={() => history.goBack()}>
                     <FormattedMessage id="uenig-selvgaende.angre"/>
                 </Knapp>
                 <Knapp className="blokk-s" type="standard" onClick={() => history.push(SBLREG_PATH)}>
@@ -24,11 +24,11 @@ function UenigSelvgaende({history}: RouteComponentProps<MatchProps>) {
                 </Knapp>
             </Knapperad>
             <div className="text-align-center">
-                <button className="avbryt--knapp-understrek" onClick={() => document.location.href = DITTNAV_URL}>
-                    <Element><FormattedMessage id="uenig-selvgaende.avbryt"/></Element>
-                </button>
+                <Link className="lenke-avbryt" to={AVBRYT_PATH}>
+                    <Element tag="span"><FormattedMessage  id="uenig-selvgaende.avbryt"/></Element>
+                </Link>
             </div>
-        </div>
+        </div >
     );
 }
 
