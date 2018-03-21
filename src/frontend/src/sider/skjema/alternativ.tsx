09b1f7a51b4f3abed779e53b40b7bbbaa6db438f
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-03-21 11:58:58 +0100

    FO-798 Lag wrapper og style knappene.

--- src/frontend/src/sider/skjema/alternativ.tsx
+++ src/frontend/src/sider/skjema/alternativ.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Radio } from 'nav-frontend-skjema';
+import { RadioPanel } from 'nav-frontend-skjema';
 import InjectedIntlProps = ReactIntl.InjectedIntlProps;
 
 interface AlternativProps {
@@ -12,14 +12,16 @@ interface AlternativProps {
 function Alternativ(props: AlternativProps & InjectedIntlProps) {
     const tekst = props.intl.messages[props.getTekstId(props.alternativId)];
     return (
-        <Radio
-            onChange={() => props.avgiSvar(props.alternativId)}
-            className="blokk-xs"
-            name={'alternativ'}
-            label={tekst}
-            value={tekst}
-            checked={props.hentAvgittSvar() === props.alternativId}
-        />
+        <div className="alternativ-wrapper">
+            <RadioPanel
+                onChange={() => props.avgiSvar(props.alternativId)}
+                className="blokk-xs"
+                name={'alternativ'}
+                label={tekst}
+                value={tekst}
+                checked={props.hentAvgittSvar() === props.alternativId}
+            />
+        </div>
     );
 }
 
