2ccc5c1fa4c8f8f4f247efdf772c2add5ae1ff86
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-05-25 09:45:14 +0200

    FO-1022 Ikke vis banner på forsiden

--- src/komponenter/banner/banner.tsx
+++ src/komponenter/banner/banner.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { Systemtittel } from 'nav-frontend-typografi';
+import { START_PATH } from '../../utils/konstanter';
 
 export default function Banner() {
-    return (
+    return (document.location.pathname.includes(START_PATH)) ? (null) : (
         <div className="registrering-banner">
             <Systemtittel tag="h1">
                 Registrering
