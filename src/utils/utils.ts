775ec5a4eb8050c3ef244eab2e67a09248efbf5b
Joselito Poblete <joselito.poblete@gmail.com>
2018-05-30 10:15:31 +0200

    FO-1021 Fikser alder

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -22,15 +22,6 @@ export function hentFornavn(name: string | undefined) {
     return name ? forsteTegnStorBokstav(name).split(' ')[0] : '';
 }
 
-export function hentAlder(personId: string) {
-
-    const fnrForsteSeksSiffer =
-        personId.substring(0, 1) === '4' ? personId.substring(1, 7) : personId.substring(0, 6) ;
-
-    const fodselsdato = moment(`${fnrForsteSeksSiffer}`, 'DDMMYY');
-    return moment().diff(fodselsdato, 'years');
-}
-
 function forsteTegnStorBokstav(name: string) {
     return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
 }
@@ -106,4 +97,52 @@ export function mapAvgitteSvarForBackend(
 
 export interface MatchProps {
     id: string;
+}
+
+/*
+* Regn ut alder basert på fnr som kommer fra `veilarboppfolging/api/me`
+* Senere kan koden under bli erstattes med at backend regner ut alder istenfor
+* */
+
+function erDNummer (personId: string) {
+    const forsteSiffer = Number(personId.substring(0, 1));
+    return forsteSiffer > 3 && forsteSiffer < 8;
+}
+
+function parseDNummer (personId: string) {
+    return !erDNummer(personId) ? personId : personId.substring(1);
+}
+
+function toSifferFodselsAar (personId: string) {
+    return personId.substring(4, 6);
+}
+
+function hentAarhundre (personId: string) {
+    let result;
+    const individNummer = Number(personId.substring(6, 9));
+    const fodselsAar = Number(personId.substring(4, 6));
+
+    if (individNummer <= 499) {
+        result = '19';
+    } else if (individNummer >= 500 && fodselsAar < 40) {
+        result = '20';
+    } else if (individNummer >= 500 && individNummer <= 749 && fodselsAar >= 54) {
+        result = '18';
+    } else if (individNummer >= 900 && fodselsAar > 39) {
+        result = '19';
+    }
+
+    return result;
+}
+export function hentAlder(personId: string) {
+
+    const fnr = parseDNummer(personId);
+
+    const aarhundre = hentAarhundre(fnr);
+    const fnrForsteFireSiffer = fnr.substring(0, 4);
+    const toSifferFAar = toSifferFodselsAar(fnr);
+
+    const fodselsdato = moment(`${fnrForsteFireSiffer}${aarhundre}${toSifferFAar}`, 'DDMMYYYY');
+
+    return moment().diff(fodselsdato, 'years');
 }
