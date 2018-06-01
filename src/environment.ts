76baca943bb37715b51c3d1c97057a6207ad53de
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-06-01 14:04:41 +0200

    FO-1107 Feil i henting av miljøvariabel

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export function getEndpointVariable() {
-    return (window as any).FEATURE_ENDPOINT_URL; // tslint:disable-line no-any
+    return (window as any).arbeidssokerregistrering.FEATURE_ENDPOINT_URL; // tslint:disable-line no-any
 }
 
 export const FEATURE_BASE_URL = getEndpointVariable();
