9f1717bbbd16cd6070a896649faf3b0a8b6cd7af
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-06-04 16:04:14 +0200

    FO-1088 Layout

--- src/sider/allerede-registrert/allerede-registrert.tsx
+++ src/sider/allerede-registrert/allerede-registrert.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Column, Container, Row } from 'nav-frontend-grid';
 import Innholdstittel from 'nav-frontend-typografi/lib/innholdstittel';
+import { Normaltekst } from 'nav-frontend-typografi';
+import GraaBakgrunn from '../../komponenter/graa-bakgrunn/graa-bakgrunn';
 
 interface Props {
 
@@ -10,27 +12,40 @@ class AlleredeRegistrert extends React.Component<Props> {
     render() {
         return (
             <Container className="allerede-registrert">
+                <GraaBakgrunn />
                 <Row>
-                    <Column xs="12" className="allerede-registrert__tittel">
-                        <Innholdstittel tag="h1">
-                            Du er allerede registrert
+                    <Column xs="12">
+                        <Innholdstittel tag="h1" className="allerede-registrert__tittel">
+                            Du er allerede registrert hos NAV
                         </Innholdstittel>
+                        <Normaltekst className="allerede-registrert__undertittel">
+                            Hva nå?
+                        </Normaltekst>
                     </Column>
                 </Row>
                 <Row>
                     <Column xs="12" sm="4" className="allerede-registrert__boks">
                         <div className="allerede-registrert__boks-innhold">
-                            Gå hit?
+                            <Normaltekst className="allerede-registrert__boks-tekst">
+                                Du kan fortsette med Aktivitetsplanen.
+                            </Normaltekst>
+                            <a href="www.nav.no" className="allerede-registrert__knapp knapp">Aktivitetsplan</a>
                         </div>
                     </Column>
                     <Column xs="12" sm="4" className="allerede-registrert__boks">
                         <div className="allerede-registrert__boks-innhold">
-                            Gå hit?
+                            <Normaltekst className="allerede-registrert__boks-tekst">
+                                Du kan finne informasjon i Veien til arbeid.
+                            </Normaltekst>
+                            <a href="www.nav.no" className="allerede-registrert__knapp knapp">Veien til arbeid</a>
                         </div>
                     </Column>
                     <Column xs="12" sm="4" className="allerede-registrert__boks">
                         <div className="allerede-registrert__boks-innhold">
-                            Gå hit?
+                            <Normaltekst className="allerede-registrert__boks-tekst">
+                                Du kan kontakte ditt lokale NAV-kontor.
+                            </Normaltekst>
+                            <a href="www.nav.no" className="allerede-registrert__knapp knapp">Dialog</a>
                         </div>
                     </Column>
                 </Row>
