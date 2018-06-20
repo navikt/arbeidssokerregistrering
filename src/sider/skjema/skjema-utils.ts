9387ed2432f51f1a06fb6681675a3db9913d2bd0
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-06-20 12:39:53 +0200

    FO-1024 Legg til test og mulighet for konfigurasjon

--- src/sider/skjema/skjema-utils.ts
+++ src/sider/skjema/skjema-utils.ts
@@ -1,24 +1,39 @@
 import { State as SvarState } from '../../ducks/svar';
 
+export type SkjemaConfig = any; // tslint:disable-line no-any
+
 export function getTekstIdForAlternativ(sporsmalId: string, alternativId: number) {
     return `${sporsmalId}-alternativ-${alternativId}`;
 }
 
-function getSporsmalSomIkkeSkalBesvares(sporsmalId: string, alternativId: number): string[] {
-    switch (sporsmalId) {
-        case 'din-situasjon': {
-            // Brukere som svarer "har ikke hatt jobb" i din-situasjon skal ikke få spørsmål om siste stilling.
-            return (alternativId === 2) ? ['siste-stilling'] : [];
-        }
-        default: {
-            return [];
-        }
+const defaultSkjemaConfig: SkjemaConfig = {
+    'din-situasjon': {
+        alternativId: 2,
+        skip: ['siste-stilling'],
+    }
+};
+
+function getSporsmalSomIkkeSkalBesvares(
+    sporsmalId: string,
+    alternativId: number,
+    skjemaConfig?: SkjemaConfig
+): string[] {
+    const config = skjemaConfig === undefined ? defaultSkjemaConfig : skjemaConfig;
+
+    if (config[sporsmalId] === undefined || config[sporsmalId].alternativId !== alternativId) {
+        return [];
+    }  else {
+        return config[sporsmalId].skip;
     }
 }
 
-export function getAlleSporsmalSomIkkeSkalBesvares(sporsmalIder: string[], svar: SvarState): string[] {
+export function getAlleSporsmalSomIkkeSkalBesvares(
+    sporsmalIder: string[],
+    svar: SvarState,
+    config?: SkjemaConfig
+): string[] {
     let sporsmal: string[] = [];
     sporsmalIder.forEach(sporsmalId =>
-        sporsmal = [...sporsmal, ...getSporsmalSomIkkeSkalBesvares(sporsmalId, svar[sporsmalId])]);
+        sporsmal = [...sporsmal, ...getSporsmalSomIkkeSkalBesvares(sporsmalId, svar[sporsmalId], config)]);
     return sporsmal;
 }
