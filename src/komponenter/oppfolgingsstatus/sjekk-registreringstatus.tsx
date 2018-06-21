ff8095c016fc548a24ec7b06d85ea519fec5bdc4
Ola <ola.galde@nav.no>
2018-06-21 13:26:03 +0200

    FO-1191: Tar i bruk egen feature-toggle for ny/gammel registrering.

--- src/komponenter/oppfolgingsstatus/sjekk-registreringstatus.tsx
+++ src/komponenter/oppfolgingsstatus/sjekk-registreringstatus.tsx
@@ -1,23 +1,25 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Data as RegistreringstatusData, selectRegistreringstatus } from '../../ducks/registreringstatus';
+import {Data as RegistreringstatusData, selectRegistreringstatus } from '../../ducks/registreringstatus';
 import { AppState } from '../../reducer';
 import SblRegistrering from '../../sider/sbl-registrering/sbl-registrering';
 import AlleredeRegistrert from '../../sider/allerede-registrert/allerede-registrert';
 import { InjectedIntlProps, injectIntl } from 'react-intl';
+import { selectBrukNyRegistreringFeatureToggle } from '../../ducks/feature-toggles';
 
 interface StateProps {
     registreringstatusData: RegistreringstatusData;
+    brukNyRegistrering: boolean;
 }
 
 type Props = StateProps & InjectedIntlProps;
 
 class SjekkRegistreringstatus extends React.PureComponent<Props> {
     render () {
-        const {registreringstatusData, children} = this.props;
+        const {registreringstatusData, children, brukNyRegistrering} = this.props;
         if (registreringstatusData.underOppfolging) {
             return <AlleredeRegistrert intl={this.props.intl} />;
-        } else if (!registreringstatusData.oppfyllerKrav) {
+        } else if (!brukNyRegistrering) {
             return <SblRegistrering/>;
         } else {
             return <>{children}</>;
@@ -27,6 +29,7 @@ class SjekkRegistreringstatus extends React.PureComponent<Props> {
 
 const mapStateToProps = (state: AppState) => ({
     registreringstatusData: selectRegistreringstatus(state).data,
+    brukNyRegistrering: selectBrukNyRegistreringFeatureToggle(state)
 });
 
 export default connect(mapStateToProps)(
