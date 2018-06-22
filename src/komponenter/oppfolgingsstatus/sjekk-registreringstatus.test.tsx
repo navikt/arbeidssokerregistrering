8d35d8f8233f65c1e9bdd5f158379dea589e582a
Ola <ola.galde@nav.no>
2018-06-22 15:23:18 +0200

    FO-1191: Fikser tester

--- src/komponenter/oppfolgingsstatus/sjekk-registreringstatus.test.tsx
+++ src/komponenter/oppfolgingsstatus/sjekk-registreringstatus.test.tsx
@@ -7,7 +7,7 @@ import * as Adapter from 'enzyme-adapter-react-16';
 import SjekkRegistreringstatus from './sjekk-registreringstatus';
 import {
     dispatchRegistreringstatus,
-    promiseWithSetTimeout, mountWithStoreAndIntl, shallowwithStoreAndIntl
+    promiseWithSetTimeout, mountWithStoreAndIntl, shallowwithStoreAndIntl, dispatchFeaturestatus
 } from '../../test/test-utils';
 import SblRegistrering from '../../sider/sbl-registrering/sbl-registrering';
 import { create } from '../../store';
@@ -27,10 +27,22 @@ describe('<SjekkRegistreringstatus />', () => {
     it('skal sende bruker til sbl om den ikke oppfyller krav og ikke er under oppfølging', () => {
         const store = create();
         dispatchRegistreringstatus({underOppfolging: false, oppfyllerKrav: false}, store);
+        dispatchFeaturestatus({'arbeidssokerregistrering.bruk-ny-registrering': false}, store);
 
         const wrapper = shallowwithStoreAndIntl(<SjekkRegistreringstatus />, store);
 
         expect(wrapper.find(SblRegistrering)).to.have.length(1);
+
+    });
+
+    it('skal ikke sende bruker til sbl om feature for ny-registrering er påskrudd', () => {
+        const store = create();
+        dispatchFeaturestatus({'arbeidssokerregistrering.bruk-ny-registrering': true}, store);
+        dispatchRegistreringstatus({underOppfolging: false, oppfyllerKrav: false}, store);
+
+        const wrapper = shallowwithStoreAndIntl(<SjekkRegistreringstatus />, store);
+
+        expect(wrapper.find(SblRegistrering)).to.have.length(0);
 
     });
 
@@ -43,9 +55,10 @@ describe('<SjekkRegistreringstatus />', () => {
 
         expect(wrapper.find(AlleredeRegistrert)).to.have.length(1);
     });
-    it('Skal rendre innhold dersom bruker oppfyller krav og ikke er under oppfølging', () => {
+    it('Skal rendre innhold dersom ny registrering er på og bruker ikke er under oppfølging', () => {
         const store = create();
 
+        dispatchFeaturestatus({'arbeidssokerregistrering.bruk-ny-registrering': true}, store);
         dispatchRegistreringstatus({underOppfolging: false, oppfyllerKrav: true}, store);
 
         const component = (
