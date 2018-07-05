b3f04e3923028d1212738fcdd80bf81c45ab706d
Kjell Arne Brødreskift <kjellabrodreskift@bekk.no>
2018-07-05 14:33:02 +0200

    FO-1258. Tatt i bruk FormattedHTMLMessage

--- src/komponenter/initialdata/stepup.tsx
+++ src/komponenter/initialdata/stepup.tsx
@@ -1,24 +1,25 @@
 import * as React from 'react';
-import { InjectedIntlProps } from 'react-intl';
+import { FormattedHTMLMessage } from 'react-intl';
 import { Normaltekst } from 'nav-frontend-typografi';
-import { getIntlMessage } from '../../utils/utils';
 import { VEILARBSTEPUP } from '../../ducks/api';
 import ResponsivSide from '../side/responsiv-side';
 import NavAlertStripe from 'nav-frontend-alertstriper';
 
-function StepUp({intl}: InjectedIntlProps) {
+function StepUp() {
     return (
         <div className="limit">
             <ResponsivSide className="stepup">
                 <NavAlertStripe type="info">
-                    <div dangerouslySetInnerHTML={{__html: getIntlMessage(intl.messages, 'stepup-melding')}}/>
+                    <FormattedHTMLMessage id="stepup-melding"/>
                 </NavAlertStripe>
                 <div className="knapperad">
                     <button
                         className="knapp knapp--hoved stepup__knapp"
                         onClick={() => document.location.href = VEILARBSTEPUP}
                     >
-                        <Normaltekst>{getIntlMessage(intl.messages, 'knapp-logg-inn')}</Normaltekst>
+                        <Normaltekst>
+                            <FormattedHTMLMessage id="knapp-logg-inn"/>
+                        </Normaltekst>
                     </button>
                 </div>
             </ResponsivSide>
