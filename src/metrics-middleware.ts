40b5ff421bfe51974d56a258455e5506f80a8d4a
Joselito Poblete <joselito.poblete@gmail.com>
2018-08-14 17:32:01 +0200

    [FO-818] FIX: test og kode som ikke er i bruk

--- src/metrics-middleware.ts
+++ src/metrics-middleware.ts
@@ -3,7 +3,10 @@ import {ActionTypes as AutentiseringsinfoActionTypes } from './ducks/autentiseri
 export const metricsMiddleWare = (store: any) => (next: any) => (action: any) => { // tslint:disable-line:no-any
     if (action.type === AutentiseringsinfoActionTypes.HENT_AUTENTISERINGSINFO_OK) {
         const { niva } = action.data;
-        (window as any).frontendlogger.event('registrering.security.level', {'Niva3': niva === 3, 'Niva4': niva === 4}, {}); // tslint:disable-line
+        const { frontendlogger } = (window as any); // tslint:disable-line
+        if (frontendlogger) {
+            frontendlogger.event('registrering.security.level', {'Niva3': niva === 3, 'Niva4': niva === 4}, {});
+        }
     }
     next(action);
 };
