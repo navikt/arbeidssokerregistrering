181ba9259d72f2e06c89da321d7c700c6a656905
Joselito Poblete <joselito.poblete@gmail.com>
2018-08-15 15:54:45 +0200

    [FO-818] Justering etter ffv

--- src/metrics-middleware.ts
+++ src/metrics-middleware.ts
@@ -3,9 +3,9 @@ import {ActionTypes as AutentiseringsinfoActionTypes } from './ducks/autentiseri
 export const metricsMiddleWare = (store: any) => (next: any) => (action: any) => { // tslint:disable-line:no-any
     if (action.type === AutentiseringsinfoActionTypes.HENT_AUTENTISERINGSINFO_OK) {
         const { niva } = action.data;
-        const { frontendlogger } = (window as any); // tslint:disable-line
+        const { frontendlogger } = (window as any).frontendlogger; // tslint:disable-line
         if (frontendlogger) {
-            frontendlogger.event('registrering.security.level', {'Niva3': niva === 3, 'Niva4': niva === 4}, {});
+            frontendlogger.event('registrering.security.level', {'niva': niva}, {});
         }
     }
     next(action);
