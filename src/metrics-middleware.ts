d61415ca1283ea3e8673767082521ea8fec37ff2
Joselito Poblete <joselito.poblete@gmail.com>
2018-08-16 14:58:09 +0200

    [FO-818] Metrikk på besvarelser

--- src/metrics-middleware.ts
+++ src/metrics-middleware.ts
@@ -1,6 +1,10 @@
 import {ActionTypes as AutentiseringsinfoActionTypes } from './ducks/autentiseringsinfo';
+import {ActionTypes as RegistrerbrukerActionTypes } from './ducks/registrerbruker';
+import { brukersSvarSamsvarerMedInfoFraAAReg } from './sider/oppsummering/oppsummering-utils';
 
 export const metricsMiddleWare = (store: any) => (next: any) => (action: any) => { // tslint:disable-line:no-any
+    const { frontendlogger } = (window as any); // tslint:disable-line
+    
     if (action.type === AutentiseringsinfoActionTypes.HENT_AUTENTISERINGSINFO_OK) {
         const { niva } = action.data;
         const { frontendlogger } = (window as any); // tslint:disable-line
@@ -8,5 +12,20 @@ export const metricsMiddleWare = (store: any) => (next: any) => (action: any) =>
             frontendlogger.event('registrering.security.level', {'niva': niva}, {});
         }
     }
+
+    let jobbetSeksAvTolvSisteManeder = store.getState().registreringStatus.data.jobbetSeksAvTolvSisteManeder;
+    if (action.type === RegistrerbrukerActionTypes.REG_BRUKER_STATUS_OK) {
+        const { besvarelse } = action.data;
+        if (frontendlogger) {
+            frontendlogger.event('registrering.besvarelse.helseHinder',
+                                 {'helseHinder': besvarelse.helseHinder}, {});
+            frontendlogger.event('registrering.besvarelse.utdanning',
+                                 {'utdanning': besvarelse.utdanning}, {});
+            frontendlogger.event('registrering.besvarelse.stillingskode',
+                                 {'stillingskode': besvarelse.sisteStilling.styrk08}, {});
+            frontendlogger.event('registrering.besvarelse.sistestilling.samsvarermedinfofraaareg', {'samsvarermedinfofraareg': brukersSvarSamsvarerMedInfoFraAAReg(besvarelse, jobbetSeksAvTolvSisteManeder)}, {}); // tslint:disable-line
+        }
+    }
+    
     next(action);
 };
