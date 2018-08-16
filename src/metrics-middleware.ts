c3123fbf1e301022fbf4fec06eda146ccc20ee22
Joselito Poblete <joselito.poblete@gmail.com>
2018-08-16 10:00:23 +0200

    [FO-818] Fix: Hent frontendlogger objektet

--- src/metrics-middleware.ts
+++ src/metrics-middleware.ts
@@ -3,7 +3,7 @@ import {ActionTypes as AutentiseringsinfoActionTypes } from './ducks/autentiseri
 export const metricsMiddleWare = (store: any) => (next: any) => (action: any) => { // tslint:disable-line:no-any
     if (action.type === AutentiseringsinfoActionTypes.HENT_AUTENTISERINGSINFO_OK) {
         const { niva } = action.data;
-        const { frontendlogger } = (window as any).frontendlogger; // tslint:disable-line
+        const { frontendlogger } = (window as any); // tslint:disable-line
         if (frontendlogger) {
             frontendlogger.event('registrering.security.level', {'niva': niva}, {});
         }
