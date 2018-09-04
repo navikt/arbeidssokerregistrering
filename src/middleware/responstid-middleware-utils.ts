def57325137923d0b6bf41a691e54db410ae2f74
Lars Andreas van Woensel Kooy Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2018-09-04 14:46:46 +0200

    FO-1478 Legg til tester

--- src/middleware/responstid-middleware-utils.ts
+++ src/middleware/responstid-middleware-utils.ts
@@ -4,15 +4,11 @@ import { Frontendlogger } from './metrics-middleware';
 const starttider = {};
 
 export function loggResponstidForTjenestekall(actionType: ActionTypes, frontendlogger: Frontendlogger) {
-
-    // tslint:disable
     const type = actionType.toString();
     if (type.includes('_PENDING')) {
         const prefix = getPrefix(type);
         starttider[prefix] = Date.now();
-        console.log(starttider);
     }
-
 
     if (type.includes('_OK') || type.includes('_FEILET')) {
         const prefix = getPrefix(type);
@@ -21,6 +17,9 @@ export function loggResponstidForTjenestekall(actionType: ActionTypes, frontendl
                 responstid: Date.now() - starttider[prefix],
             }, {});
         }
+        console.log(getEventString(prefix), {
+            responstid: Date.now() - starttider[prefix],
+        });
     }
 }
 
