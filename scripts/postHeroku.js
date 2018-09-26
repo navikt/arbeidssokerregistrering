b82066b5f658c2fcb9afff06d2ede34b6b405406
Joselito <joselito.poblete@gmail.com>
2018-09-26 20:46:26 +0200

    Update postHeroku.js

--- scripts/postHeroku.js
+++ scripts/postHeroku.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
 const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
-pkg.homepage = '/veilarbmaofs';
+pkg.homepage = '/arbeidssokerregistrering';
 
 fs.writeFileSync('./package.json', JSON.stringify(pkg, null, 2), 'utf-8');
