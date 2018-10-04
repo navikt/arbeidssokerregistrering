731663a87cc289968713dcfba838800b8446e160
Joselito Poblete <joselito.poblete@gmail.com>
2018-10-04 09:21:36 +0200

    [FO-1532] Egen katalog for heroku

--- heroku/index.js
+++ heroku/index.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+    res.sendFile(path.join(__dirname + '/../build/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
