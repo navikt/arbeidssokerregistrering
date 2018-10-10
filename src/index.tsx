791f912581dc8ae337c43dd14c1813e85fb61b83
Joselito Poblete <joselito.poblete@gmail.com>
2018-10-10 16:04:12 +0200

    [KAIZEN-0] hotjar for herokuapp

--- src/index.tsx
+++ src/index.tsx
@@ -12,10 +12,11 @@ if (process.env.REACT_APP_MOCK === 'true') {
     console.log('=========================='); /*tslint:disable-line:no-console*/
 
     const s = document.createElement('script');
+    // tslint:disable
+    const code = "(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:1045017,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"
     s.async = true;
-    s.src = 'https://static.hotjar.com/c/hotjar-1045017.js?sv=6';
+    s.appendChild(document.createTextNode(code));
     document.body.appendChild(s);
-
     require('./mocks/mocks');
 }
 
