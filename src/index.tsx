75bd9b1863f73720e4eebfd63edcef9696d6c989
Joselito Poblete <joselito.poblete@gmail.com>
2018-10-11 15:53:26 +0200

    [KAIZEN-0] Heroku med hotjar

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,17 @@ if (process.env.REACT_APP_MOCK === 'true') {
     console.log('=========================='); /*tslint:disable-line:no-console*/
     console.log('======== MED MOCK ========'); /*tslint:disable-line:no-console*/
     console.log('=========================='); /*tslint:disable-line:no-console*/
+
+    const s = document.createElement('script');
+    const code = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n" +
+        "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n" +
+        "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n" +
+        "'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n" +
+        "})(window,document,'script','dataLayer','GTM-T5HQ63');\n";
+    s.async = true;
+    s.appendChild(document.createTextNode(code));
+    document.body.appendChild(s);
+
     require('./mocks/mocks');
 }
 
