abbd6c75d9eaa472bd4c623c4492716280056cc8
Joselito Poblete <joselito.poblete@gmail.com>
2018-10-11 09:24:01 +0200

    Formatering

--- integration/globals.js
+++ integration/globals.js
@@ -4,23 +4,26 @@ module.exports = {
     default: {
         isLocal: true,
     },
-    before: function(done) {
-        if (this.isLocal) {mock.startMock();
-        chromedriver.start();
-        setTimeout(function() {
-            done();
-        }, 10000);
-    }else {
+    before: function (done) {
+        if (this.isLocal) {
+            mock.startMock();
+            chromedriver.start();
+            setTimeout(function () {
+                done();
+            }, 10000);
+        } else {
             done();
 
         }
     },
-    after: function(done) {
-        if (this.isLocal) {mock.stopMock();
-        chromedriver.stop();
-        setTimeout(function() {
-            done();
-        }, 200);} else {
+    after: function (done) {
+        if (this.isLocal) {
+            mock.stopMock();
+            chromedriver.stop();
+            setTimeout(function () {
+                done();
+            }, 200);
+        } else {
             done();
         }
     },
