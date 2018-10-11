cb71c27e78cc7ccba25644cc803e9e4b4183f907
Joselito Poblete <joselito.poblete@gmail.com>
2018-10-11 16:18:21 +0200

    [KAIZEN-0] Formattering lint feil

--- src/index.tsx
+++ src/index.tsx
@@ -15,11 +15,11 @@ if (process.env.REACT_APP_MOCK === 'true') {
     /* Hotjar script som bruke i herokuapp for brukertesting */
     /* Dataen er tilgjengelig under organisasjon navlab.no i NAV hotjar */
     const s = document.createElement('script');
-    const code = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n" +
-        "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n" +
-        "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n" +
-        "'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n" +
-        "})(window,document,'script','dataLayer','GTM-T5HQ63');\n";
+    const code = '(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':\n' +
+        'new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],\n' +
+        'j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=\n' +
+        '\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);\n' +
+        '})(window,document,\'script\',\'dataLayer\',\'GTM-T5HQ63\');\n';
     s.async = true;
     s.appendChild(document.createTextNode(code));
     document.body.appendChild(s);
