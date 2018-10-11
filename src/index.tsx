5242a01c04d7099921e58e7bf675048364aee30b
Joselito Poblete <joselito.poblete@gmail.com>
2018-10-11 15:45:23 +0200

    [KAIZEN-0] Heroku med hotjar

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,11 @@ if (process.env.REACT_APP_MOCK === 'true') {
     console.log('=========================='); /*tslint:disable-line:no-console*/
 
     const s = document.createElement('script');
-    // tslint:disable
-    const code = "(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:1045017,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"
+    const code = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\n" +
+        "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\n" +
+        "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n" +
+        "'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n" +
+        "})(window,document,'script','dataLayer','GTM-T5HQ63');\n";
     s.async = true;
     s.appendChild(document.createTextNode(code));
     document.body.appendChild(s);
