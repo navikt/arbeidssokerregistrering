fe0cae413fb1a8284eb2ab90cf4659913e9777ed
Joselito Poblete <joselito.poblete@gmail.com>
2018-10-17 13:05:29 +0200

    [KAIZEN-0] Fix: igjen øke timeout

--- integration/tests/navigering-alle-spm.int.js
+++ integration/tests/navigering-alle-spm.int.js
@@ -47,20 +47,20 @@ module.exports = {
     'oppsummering-siden skal besta av tittel og oppsummering infoboks': (client) => {
 
         client.saveScreenshot(`integration/reports/oppsummering.png`);
-        sporsmal.validerSiden('@tittelOppsummeringTittel', '@divOppsummeringBesvarelser', 40000);
+        sporsmal.validerSiden('@tittelOppsummeringTittel', '@divOppsummeringBesvarelser', 10000);
         sporsmal.klikkNeste();
     },
     'fullfor-siden skal besta av tittel og sjekkliste infoboks': (client) => {
 
         client.saveScreenshot(`integration/reports/fullfor.png`);
-        sporsmal.validerSiden('@tittelFullforTittel', '@divFullforSjekkliste', 40000);
+        sporsmal.validerSiden('@tittelFullforTittel', '@divFullforSjekkliste', 10000);
         sporsmal.klikkSjekkboksFullfor();
         sporsmal.klikkNeste();
     },
     'duernaregistert-siden skal besta av tittel og infoboks': (client) => {
 
         client.saveScreenshot(`integration/reports/duernaregistrert.png`);
-        sporsmal.validerSiden('@tittelDuErRegTittel', '@divDuErReg', 40000);
+        sporsmal.validerSiden('@tittelDuErRegTittel', '@divDuErReg', 10000);
     }
 
 };
