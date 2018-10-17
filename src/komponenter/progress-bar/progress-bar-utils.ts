154fcb16684a9675b763edf156527755b1b535f8
Alexander Gård <alexander.gard@nav.no>
2018-10-17 16:34:32 +0200

    FO-1605 & FO-1606 Fikset progress bar til å fungee på de nye løpene

--- src/komponenter/progress-bar/progress-bar-utils.ts
+++ src/komponenter/progress-bar/progress-bar-utils.ts
@@ -1,6 +1,19 @@
-import { FULLFOR_PATH, SKJEMA_PATH, basename, OPPSUMMERING_PATH, DUERNAREGISTRERT_PATH } from '../../utils/konstanter';
+import {
+    FULLFOR_PATH,
+    SKJEMA_PATH,
+    basename,
+    OPPSUMMERING_PATH,
+    DUERNAREGISTRERT_PATH,
+    SKJEMA_SYKEFRAVAER_PATH, INNGANGSSPORSMAL
+} from '../../utils/konstanter';
 
-export const progressBarConfig: string[] = [
+const etterSporsmalConfig: string[] = [
+    OPPSUMMERING_PATH,
+    FULLFOR_PATH,
+    DUERNAREGISTRERT_PATH
+];
+
+const progressBarConfig: string[] = [
     `${SKJEMA_PATH}/0`,
     `${SKJEMA_PATH}/1`,
     `${SKJEMA_PATH}/2`,
@@ -8,7 +21,52 @@ export const progressBarConfig: string[] = [
     `${SKJEMA_PATH}/4`,
     `${SKJEMA_PATH}/5`,
     `${SKJEMA_PATH}/6`,
-    OPPSUMMERING_PATH,
-    FULLFOR_PATH,
-    DUERNAREGISTRERT_PATH,
+    ...etterSporsmalConfig
 ].map(path => `${basename}${path}`);
+
+const nyArbeidsgiverConfig: string[] = lagConfigForSykefravaerLop(1, 5);
+
+const sammeArbeidsgiverConfig: string[] = lagConfigForSykefravaerLop(2, 5);
+
+const usikkerConfig: string[] = lagConfigForSykefravaerLop(3, 5);
+
+const ingenPasserConfig: string[] = lagConfigForSykefravaerLop(4, 5);
+
+function lagConfigForSykefravaerLop(lop: number, sporsmal: number): string[] {
+    const config: string[] = [];
+
+    config.push(INNGANGSSPORSMAL);
+
+    for (let i = 0; i <= sporsmal; i++) {
+        config.push(`${SKJEMA_SYKEFRAVAER_PATH}/${lop}/${i}`);
+    }
+
+    config.push(...etterSporsmalConfig);
+
+    return config;
+}
+
+export function finnRiktigConfig(pathName: string): string[] {
+
+    if (progressBarConfig.includes(pathName)) {
+        return progressBarConfig;
+    }
+
+    if (nyArbeidsgiverConfig.includes(pathName)) {
+        return nyArbeidsgiverConfig;
+    }
+
+    if (sammeArbeidsgiverConfig.includes(pathName)) {
+        return sammeArbeidsgiverConfig;
+    }
+
+    if (usikkerConfig.includes(pathName)) {
+        return usikkerConfig;
+    }
+
+    if (ingenPasserConfig.includes(pathName)) {
+        return ingenPasserConfig;
+    }
+
+    return [];
+}
