5b9e06d1aac743b4b8dc1da03969f662a62f1ebe
Joselito Poblete <joselito.poblete@gmail.com>
2018-10-24 11:06:15 +0200

    [FO-1646] Fix: test etter navnendring

--- src/komponenter/oppfolgingsstatus/sjekk-registreringstatus.test.tsx
+++ src/komponenter/oppfolgingsstatus/sjekk-registreringstatus.test.tsx
@@ -16,7 +16,7 @@ import SblRegistrering from '../../sider/sbl-registrering/sbl-registrering';
 import {create} from '../../store';
 import AlleredeRegistrert from '../../sider/allerede-registrert/allerede-registrert';
 import InfoForIkkeArbeidssokerUtenOppfolging from "../../sider/info-for-ikke-arbeidssoker-uten-oppfolging/info-for-ikke-arbeidssoker-uten-oppfolging";
-import {Registreringstatus} from "../../ducks/registreringstatus";
+import {Registreringstatus, RegistreringType} from "../../ducks/registreringstatus";
 
 enzyme.configure({adapter: new Adapter()});
 
@@ -62,7 +62,7 @@ describe('<SjekkRegistreringstatus />', () => {
     it('skal sende bruker til AlleredeRegistrert om den er under oppfølging', () => {
         const store = create();
 
-        dispatchRegistreringstatus({underOppfolging: true, registreringStatus: Registreringstatus.ALLEREDE_REGISTRERT}, store);
+        dispatchRegistreringstatus({underOppfolging: true, registreringType: RegistreringType.ALLEREDE_REGISTRERT}, store);
 
         const wrapper = mountWithStoreRouterAndIntl(<SjekkRegistreringstatus/>, store);
 
@@ -72,7 +72,7 @@ describe('<SjekkRegistreringstatus />', () => {
     it('skal sende bruker til InfoForIkkeArbeidssokerUtenOppfolging om status er erIkkeArbeidssokerUtenOppfolging', () => {
         const store = create();
 
-        dispatchRegistreringstatus({erIkkeArbeidssokerUtenOppfolging: true, registreringStatus: Registreringstatus.SPERRET}, store);
+        dispatchRegistreringstatus({erIkkeArbeidssokerUtenOppfolging: true, registreringType: RegistreringType.SPERRET}, store);
 
         const wrapper = mountWithStoreRouterAndIntl(<SjekkRegistreringstatus/>, store);
 
