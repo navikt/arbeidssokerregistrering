8ce6a51125c912e53eca0b4a3cb0a32cdfc908a3
Joselito Poblete <joselito.poblete@gmail.com>
2018-11-13 15:30:46 +0100

    [KAIZEN-0] Fjernet ubrukt filer

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './app';
 
 import './index.less';
 
-if (process.env.REACT_APP_MOCK === 'true') {
+if (process.env.REACT_APP_MOCK) {
     console.log('=========================='); /*tslint:disable-line:no-console*/
     console.log('======== MED MOCK ========'); /*tslint:disable-line:no-console*/
     console.log('=========================='); /*tslint:disable-line:no-console*/
