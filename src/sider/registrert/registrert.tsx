049c1e37630b98ec2109c96f69c94561a2ff201a
Alexander Gård <alexanog@stud.ntnu.no>
2018-11-19 16:15:24 +0100

    FO-1655 La til tekster for registrert-siden for sykmeldte

--- src/sider/registrert/registrert.tsx
+++ src/sider/registrert/registrert.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { FormattedMessage, injectIntl, InjectedIntlProps } from 'react-intl';
 import { Normaltekst, Systemtittel, Element } from 'nav-frontend-typografi';
-import { VEIENTILARBEID_MED_DAGPENGER_URL, VEIENTILARBEID_URL } from '../../ducks/api';
+import { VEIENTILARBEID_MED_AAP_URL, VEIENTILARBEID_MED_DAGPENGER_URL, VEIENTILARBEID_URL } from '../../ducks/api';
 import AvsjekkBilde from './avsjekk-bilde';
 import { erIE } from '../../utils/ie-test';
 import { frontendLogger } from '../../metrikker/metrics-utils';
@@ -12,6 +12,7 @@ import { AppState } from '../../reducer';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
 import { MatchProps } from '../../utils/utils';
+import { RegistreringType } from '../../ducks/registreringstatus';
 
 interface StateProps {
     state: AppState;
@@ -21,14 +22,28 @@ type AllProps = StateProps & InjectedIntlProps & RouteComponentProps<MatchProps>
 
 class DuErNaRegistrert extends React.Component<AllProps> {
 
+    hentTekstId(erSykmeldt: boolean): (id: string) => string {
+        return (id: string) => {
+            return `duernaregistrert-${( erSykmeldt ? 'sykmeldt' : 'ordinaer')}-${id}`;
+        };
+    }
+
     render() {
+
+        const registreringType = this.props.state.registreringStatus.data.registreringType;
+        const erSykmeldt = registreringType === RegistreringType.SYKMELDT_REGISTRERING;
+        const hentTekstId = this.hentTekstId(erSykmeldt);
+        const veilenTilArbeidMedUrl = erSykmeldt ? VEIENTILARBEID_MED_AAP_URL : VEIENTILARBEID_MED_DAGPENGER_URL;
+
+        console.log(erSykmeldt); // tslint:disable-line
+
         return (
             <section className={`registrert ${erIE() && 'erIE'}`}>
 
                 <div className="registrert__avsjekk">
                     <AvsjekkBilde/>
                     <Systemtittel tag="h1" className="registrert__tittel">
-                        <FormattedMessage id="duernaregistrert-innholdstittel"/>
+                        <FormattedMessage id={hentTekstId('innholdstittel')}/>
                     </Systemtittel>
                 </div>
 
@@ -36,13 +51,13 @@ class DuErNaRegistrert extends React.Component<AllProps> {
                     <img src={handinfoSvg} alt="Hånd med info skilt" className="registrert__handinfo-ikon"/>
                     <div className="registrert__tekster">
                         <Systemtittel tag="h2" className="blokk-xs">
-                            <FormattedMessage id="duernaregistrert-systemtittel"/>
+                            <FormattedMessage id={hentTekstId('systemtittel')}/>
                         </Systemtittel>
                         <Normaltekst className="blokk">
-                            <FormattedMessage id="duernaregistrert-normaltekst"/>
+                            <FormattedMessage id={hentTekstId('normaltekst')}/>
                         </Normaltekst>
                         <Element className="blokk-xxs">
-                            <FormattedMessage id="duernaregistrert-element"/>
+                            <FormattedMessage id={hentTekstId('element')}/>
                         </Element>
                         <div className="registrert__knapperad">
                             <a
@@ -55,7 +70,7 @@ class DuErNaRegistrert extends React.Component<AllProps> {
                                 <FormattedMessage id="knapp-ikke-na"/>
                             </a>
                             <a
-                                href={VEIENTILARBEID_MED_DAGPENGER_URL}
+                                href={veilenTilArbeidMedUrl}
                                 className="registrert__lenke knapp knapp--hoved"
                                 onClick={() => {
                                     frontendLogger('registrering.vis.dagpenger.info');
