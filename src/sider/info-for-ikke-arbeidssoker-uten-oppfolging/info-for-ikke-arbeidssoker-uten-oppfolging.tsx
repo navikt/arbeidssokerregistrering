87dfaef2bac66957a647329f40d38bb211d6594f
Alexander Gård <alexanog@stud.ntnu.no>
2018-11-19 11:58:51 +0100

    FO-1711 Oppdaterte designet til info-for-ikke-arbeidssoker

--- src/sider/info-for-ikke-arbeidssoker-uten-oppfolging/info-for-ikke-arbeidssoker-uten-oppfolging.tsx
+++ src/sider/info-for-ikke-arbeidssoker-uten-oppfolging/info-for-ikke-arbeidssoker-uten-oppfolging.tsx
@@ -1,31 +1,42 @@
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
-import { Normaltekst } from 'nav-frontend-typografi';
-import Veilederpanel from 'nav-frontend-veilederpanel';
-
+import Banner from '../../komponenter/banner/banner';
 import utropstegnSvg from '../fullfor/utropstegn.svg';
+import { Normaltekst } from 'nav-frontend-typografi';
+import { FormattedMessage } from 'react-intl';
+import LenkeMedChevron from '../../komponenter/lenke-med-chevron/lenke-med-chevron';
+import { injectIntl } from 'react-intl';
 import './info-for-ikke-arbeidssoker-uten-oppfolging.less';
+import InjectedIntlProps = ReactIntl.InjectedIntlProps;
 
-class InfoForIkkeArbeidssokerUtenOppfolging extends React.Component {
+class InfoForIkkeArbeidssokerUtenOppfolging extends React.Component<InjectedIntlProps> {
     render() {
+
+        const lenkeUrl = this.props.intl.messages['info-for-ikke-arbeidssoker-uten-oppfolging-lenke-url'];
+
         return (
-            <div className="info-for-ikke-arbeidssoker">
-                <Veilederpanel
-                    type="plakat"
-                    svg={<img
-                        src={utropstegnSvg}
-                        alt="Informasjon"
-                        className="nav-veilederpanel__illustrasjon"
-                    />}
-                    kompakt={true}
-                >
-                    <Normaltekst>
-                        <FormattedMessage id="info-for-ikke-arbeidssoker-uten-oppfolging-innhold"/>
-                    </Normaltekst>
-                </Veilederpanel>
-            </div>
+            <>
+                <Banner/>
+                <div className="info-for-ikke-arbeidssoker">
+                    <div className="info-boks">
+                        <img
+                            src={utropstegnSvg}
+                            alt="Informasjon"
+                            className="info-boks--bilde"
+                        />
+                        <div className="info-boks--innhold">
+                            <Normaltekst className="blokk-m">
+                                <FormattedMessage id="info-for-ikke-arbeidssoker-uten-oppfolging-innhold"/>
+                            </Normaltekst>
+                            <LenkeMedChevron path={lenkeUrl}>
+                                <FormattedMessage id="info-for-ikke-arbeidssoker-uten-oppfolging-lenke-tekst"/>
+                            </LenkeMedChevron>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
+
     }
 }
 
-export default InfoForIkkeArbeidssokerUtenOppfolging;
+export default injectIntl(InfoForIkkeArbeidssokerUtenOppfolging);
