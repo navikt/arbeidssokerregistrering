04c30bda566e328de91df1ff42e6f17e19eac20b
Izis Frøyhov <izis.froyhov@nav.no>
2018-12-04 12:55:20 +0100

    KAIZEN-0: Fjerne mobil chrome

--- integration/browserstack-runner.js
+++ integration/browserstack-runner.js
@@ -19,7 +19,7 @@ utils.getNetworkIp().then(ip => {
             return runner
                 .startApp("cross-env PORT=4001 REACT_APP_MOCK_BES=true REACT_APP_MOCK=true npm start", 20000)
                 .src(['integration/registrering.test.ts', 'integration/uu.test.ts'])
-                .browsers(['browserstack:iPhone XS@12.0', 'browserstack:chrome:Windows 10', 'browserstack:ie@11.0:Windows 8.1', "browserstack:Samsung Galaxy S9@8.0"])
+                .browsers(['browserstack:iPhone XS@12.0', 'browserstack:chrome:Windows 10', 'browserstack:ie@11.0:Windows 8.1'])
                 .screenshots('./integration/screenshots/', true, '${BROWSER}_${TEST}.png')
                 .run();
         })
