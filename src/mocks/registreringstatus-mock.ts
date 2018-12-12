2ffb5560a3525c28cf91ce9c987df4f2d0e396b9
Joselito Poblete <joselito.poblete@gmail.com>
2018-12-12 16:32:01 +0100

    [FO-1774] Fiks mock

--- src/mocks/registreringstatus-mock.ts
+++ src/mocks/registreringstatus-mock.ts
@@ -4,5 +4,5 @@ export default {
     underOppfolging: false,
     jobbetSeksAvTolvSisteManeder: false,
     sykmeldtFraDato: '21.10.2018',
-    registreringType: RegistreringType.SYKMELDT_REGISTRERING
+    registreringType: RegistreringType.ORDINAER_REGISTRERING
 };
