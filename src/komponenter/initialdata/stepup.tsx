f165ab0258365fbfb60ad72f96141f0b09c7b16f
matiasvinjevoll <matias.vinjevoll@nav.no>
2018-12-13 17:16:39 +0100

    FO-1810: Ta bort lenke til MinId

--- src/komponenter/initialdata/stepup.tsx
+++ src/komponenter/initialdata/stepup.tsx
@@ -1,26 +1,27 @@
 import * as React from 'react';
-import { FormattedHTMLMessage } from 'react-intl';
-import { Normaltekst } from 'nav-frontend-typografi';
-import NavAlertStripe from 'nav-frontend-alertstriper';
+import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
+import { Innholdstittel, Normaltekst } from 'nav-frontend-typografi';
 import { VEILARBSTEPUP } from '../../ducks/api';
-
 import './stepup.less';
 
 function StepUp() {
     return (
         <section className="stepup">
-            <NavAlertStripe type="info">
-                <FormattedHTMLMessage id="stepup-melding"/>
-            </NavAlertStripe>
-            <div className="knapperad">
-                <button
-                    className="knapp knapp--hoved stepup__knapp"
-                    onClick={() => window.location.href = VEILARBSTEPUP}
-                >
-                    <Normaltekst>
-                        <FormattedHTMLMessage id="knapp-logg-inn"/>
-                    </Normaltekst>
-                </button>
+            <div className="limit">
+                <Innholdstittel className="tittel">
+                    <FormattedMessage id="overskrift-stepup"/>
+                </Innholdstittel>
+                <Normaltekst className="beskrivelse"><FormattedMessage id="stepup-melding"/></Normaltekst>
+                <div className="knapperad">
+                    <button
+                        className="knapp knapp--hoved stepup__knapp"
+                        onClick={() => window.location.href = VEILARBSTEPUP}
+                    >
+                        <Normaltekst>
+                            <FormattedHTMLMessage id="knapp-logg-inn"/>
+                        </Normaltekst>
+                    </button>
+                </div>
             </div>
         </section>
     );
