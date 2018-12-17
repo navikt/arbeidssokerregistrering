2016e1f4f8cd7813e3385a740fe410679a038124
matiasvinjevoll <matias.vinjevoll@nav.no>
2018-12-17 16:58:37 +0100

    FO-1810: Refaktorer stepup med ny info-kort komponent

--- src/komponenter/initialdata/stepup.tsx
+++ src/komponenter/initialdata/stepup.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
-import { Normaltekst, Undertittel, Systemtittel } from 'nav-frontend-typografi';
+import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import { VEILARBSTEPUP } from '../../ducks/api';
 import './stepup.less';
 import { InjectedIntlProps } from 'react-intl';
-import { HoyreChevron } from 'nav-frontend-chevron';
 import bankidSvg from './bankid.svg';
 import navKontorSvg from './nav-kontor.svg';
+import InfoKort from '../info-kort/info-kort';
 
-const StepUp: React.SFC<InjectedIntlProps> = (props: InjectedIntlProps ) => {
+const StepUp: React.SFC<InjectedIntlProps> = (props: InjectedIntlProps) => {
     const navKontorLenke = props.intl.messages['finn-ditt-nav-kontor-lenke-url'];
     return (
         <section className="stepup">
@@ -18,54 +18,40 @@ const StepUp: React.SFC<InjectedIntlProps> = (props: InjectedIntlProps ) => {
                 </Systemtittel>
                 <Normaltekst className="beskrivelse"><FormattedMessage id="stepup-melding"/></Normaltekst>
                 <div className="knapper-vertikalt">
-                    <button
+                    <a
                         className="knapp knapp--hoved stepup__knapp"
-                        onClick={() => window.location.href = VEILARBSTEPUP}
+                        href={VEILARBSTEPUP}
                     >
                         <Normaltekst>
                             <FormattedHTMLMessage id="knapp-logg-inn"/>
                         </Normaltekst>
-                    </button>
+                    </a>
                 </div>
             </div>
             <div className="stepup__hjelp">
-                <div className="limit">
-                <Systemtittel className="tittel">
-                    <FormattedMessage id="overskrift-hjelp-stepup"/>
-                </Systemtittel>
-                <div className="stepup__hjelp__informasjon">
-                    <div>
-                        <img src={bankidSvg} alt={'BankID'}/>
-                        <Undertittel><FormattedMessage id="overskrift-elektronisk-hjelp-stepup"/></Undertittel>
-                        <Normaltekst className="beskrivelse">
-                            <FormattedMessage id="beskrivelse-elektronisk-hjelp-stepup"/>
-                        </Normaltekst>
-                        <a
-                            className="hjelp-lenke typo-element"
-                            href="https://eid.difi.no/nb/bankid"
-                            target="_blank"
-                        >
-                            Skaff deg elektronisk ID
-                        </a>
-                        <HoyreChevron/>
-                    </div>
-                    <div>
-                        <img src={navKontorSvg} alt={'NAV-kontor'}/>
-                        <Undertittel><FormattedMessage id="overskrift-manuell-hjelp-stepup"/></Undertittel>
-                        <Normaltekst className="beskrivelse">
-                            <FormattedMessage id="beskrivelse-manuell-hjelp-stepup"/>
-                        </Normaltekst>
-                        <a
-                            className="hjelp-lenke typo-element"
-                            href={navKontorLenke}
-                            target="_blank"
-                        >
-                            Finn ditt lokale NAV-kontor
-                        </a>
-                        <HoyreChevron/>
+                <div className="stepup__hjelp__innhold limit">
+                    <Systemtittel className="tittel">
+                        <FormattedMessage id="overskrift-hjelp-stepup"/>
+                    </Systemtittel>
+                    <div className="stepup__hjelp__kort">
+                        <InfoKort
+                            bilde={bankidSvg}
+                            bildeBeskrivelse="BankID illustrasjon"
+                            tittel="overskrift-elektronisk-hjelp-stepup"
+                            beskrivelse="beskrivelse-elektronisk-hjelp-stepup"
+                            lenke="https://eid.difi.no/nb/bankid"
+                            lenkeTekst="Skaff deg elektronisk ID"
+                        />
+                        <InfoKort
+                            bilde={navKontorSvg}
+                            bildeBeskrivelse="NAV-kontor illustrasjon"
+                            tittel="overskrift-manuell-hjelp-stepup"
+                            beskrivelse="beskrivelse-manuell-hjelp-stepup"
+                            lenke={navKontorLenke}
+                            lenkeTekst="Finn ditt lokale NAV-kontor"
+                        />
                     </div>
                 </div>
-            </div>
             </div>
         </section>
     );
