2d8f144c1a5c8f6ce9392d9854b5f2b921c5abf1
Izis Frøyhov <izis.froyhov@nav.no>
2019-01-03 09:08:45 +0100

    FO-1826: Legge inn sjekk pa kjorende browserstack sesjoner

--- integration/browserstack-runner.js
+++ integration/browserstack-runner.js
@@ -1,6 +1,7 @@
 const createTestCafe = require('testcafe');
 const utils = require('./getNetworkIp');
-
+const browserstack = require("browserstack");
+const browsers = ['browserstack:iPhone XS@12.1', 'browserstack:chrome:Windows 10', 'browserstack:ie@11.0:Windows 8.1', 'browserstack:Samsung Galaxy S9@8.0'];
 const pullRequest = process.env.TRAVIS_PULL_REQUEST || false;
 const branch = pullRequest
     ? process.env.TRAVIS_PULL_REQUEST_BRANCH
@@ -9,30 +10,54 @@ const branch = pullRequest
 process.env.BROWSERSTACK_PROJECT_NAME = 'arbeidssokerregistrering';
 process.env.BROWSERSTACK_BUILD_ID = branch;
 
-utils.getNetworkIp().then(ip => {
+async function getRunningBrowserstackSessions() {
+    const browserStackCredentials = {
+        username: process.env.BROWSERSTACK_USERNAME,
+        password: process.env.BROWSERSTACK_ACCESS_KEY
+    };
+    const client = browserstack.createClient(browserStackCredentials);
+    const workerStatus = await new Promise(function(resolve, reject) {
+        client.getApiStatus((error, workers) => {
+            if (error) reject(error);
+            else resolve(workers);
+        });
+    });
+    return workerStatus;
+}
 
-    console.log("Starter testcafe");
-    createTestCafe(ip, 1337, 1338)
-        .then(testcafe => {
+getRunningBrowserstackSessions()
+    .then(bsStatus => {
+        const requiredSessions = browsers.length;
+        const remainingSessions = bsStatus.sessions_limit - bsStatus.running_sessions;
+        console.log(`Browserstack: There are ${bsStatus.running_sessions}/${bsStatus.sessions_limit} browserstack sessions running`);
 
-            testcafe
-                .createRunner()
-                .startApp("cross-env PORT=4001 REACT_APP_MOCK_BES=true REACT_APP_MOCK=true npm start", 20000)
-                .src(['integration/registrering.test.ts', 'integration/uu.test.ts'])
-                .browsers(['browserstack:iPhone XS@12.0', 'browserstack:chrome:Windows 10', 'browserstack:ie@11.0:Windows 8.1', 'browserstack:Samsung Galaxy S9@8.0'])
-                .screenshots('./integration/screenshots/', true, '${BROWSER}_${TEST}.png')
-                .run()
-                .then(failedCount => {
-                    console.log('Tests failed: ' + failedCount);
-                    testcafe.close();
-                    if(failedCount > 0) process.exit(1);
-                })
-                .catch(e => {
-                    console.log("Runner error: " + e);
-                    testcafe.close();
-                    process.exit(1);
+        if(remainingSessions < requiredSessions) {
+            console.log(`Browserstack: There are not enough avaliable sessions. Test suite requires <${requiredSessions}> `);
+            process.exit(1);
+        }
+    })
+    .then(() => {
+        utils.getNetworkIp().then(ip => {
 
-                });
+            console.log("Starter testcafe");
+            createTestCafe(ip, 1337, 1338).then(testcafe => {
+                testcafe
+                    .createRunner()
+                    .startApp("cross-env PORT=4001 REACT_APP_MOCK_BES=true REACT_APP_MOCK=true npm start", 20000)
+                    .src(['integration/registrering.test.ts', 'integration/uu.test.ts'])
+                    .browsers(browsers)
+                    .screenshots('./integration/screenshots/', true, '${BROWSER}_${TEST}.png')
+                    .run()
+                    .then(failedCount => {
+                        console.log('Tests failed: ' + failedCount);
+                        testcafe.close();
+                        if(failedCount > 0) process.exit(1);
+                    })
+                    .catch(e => {
+                        console.log("Runner error: " + e);
+                        testcafe.close().then(() => process.exit(1));
+                    });
+            })
+
         })
-
-});
+    });
