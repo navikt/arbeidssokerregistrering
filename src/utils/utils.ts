7f22b9b431b0f09df89d59068572453048b43d30
Joselito Poblete <joselito.poblete@gmail.com>
2019-01-21 13:24:47 +0100

    [KAIZEN-0] Endre format på maksdato

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -72,7 +72,7 @@ export function formaterDato(dato: string | undefined) {
         return '';
     }
 
-    return moment(dato, 'DD.MM.YYYY').format('DD. MMMM YYYY');
+    return moment(dato, 'YYYY-MM-DD').format('DD. MMMM YYYY');
 }
 
 export function hentAlder(personId: string) {
