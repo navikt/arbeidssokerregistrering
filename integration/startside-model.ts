68531af66c49c00130f702fb648de6613995811b
Joselito Poblete <joselito.poblete@gmail.com>
2019-02-20 15:19:12 +0100

    FO-1940 Fiks integrasjonstest

--- integration/startside-model.ts
+++ integration/startside-model.ts
@@ -1,11 +1,12 @@
 import { Selector } from 'testcafe';
+import { erIFSS } from '../src/utils/utils';
 
 export default class Startside {
     side: Selector;
     btnStartRegistrering: Selector;
 
     constructor () {
-        this.side = Selector('.registrering-arbeidssoker');
+        this.side = erIFSS() ? Selector('.registrering-arbeidssoker') : Selector('.startside') ;
         this.btnStartRegistrering = Selector('[data-testid="start-registrering"]');
     }
 }
