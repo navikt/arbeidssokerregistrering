0d18f7beca61c947d670ef91d11e178f3b7dd54a
Joselito Poblete <joselito.poblete@gmail.com>
2019-02-21 12:22:33 +0100

    FO-1940 Feil i integrasjonstest

--- integration/startside-model.ts
+++ integration/startside-model.ts
@@ -1,12 +1,11 @@
 import { Selector } from 'testcafe';
-import { erIFSS } from '../src/utils/utils';
 
 export default class Startside {
     side: Selector;
     btnStartRegistrering: Selector;
 
     constructor () {
-        this.side = erIFSS() ? Selector('.registrering-arbeidssoker') : Selector('.startside') ;
+        this.side = Selector('.startside') ;
         this.btnStartRegistrering = Selector('[data-testid="start-registrering"]');
     }
 }
