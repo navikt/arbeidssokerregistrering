074a6dc7e6e3c1765ef3984605cb1068a74941e0
Joselito Poblete <joselito.poblete@gmail.com>
2019-04-16 16:09:10 +0200

    FO-2175 Fiks tester og import

--- src/sider/fullfor/fullfor-utils.test.tsx
+++ src/sider/fullfor/fullfor-utils.test.tsx
@@ -1,7 +1,6 @@
 /*tslint:disable*/
-import {expect} from 'chai';
 import * as enzyme from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16';
 import {sisteStillingErSatt} from "./fullfor-utils";
 import {annenStilling, ingenYrkesbakgrunn, tomStilling} from "../../ducks/siste-stilling";
 import {sisteStillingMock} from "../../mocks/siste-stilling-mock";
@@ -15,11 +14,11 @@ afterEach(() => {
 
 describe('Test fullfor-utils', () => {
     it('test sisteStillingErSatt', () => {
-        expect(sisteStillingErSatt(tomStilling)).to.equal(false);
-        expect(sisteStillingErSatt(undefined)).to.equal(false);
-        expect(sisteStillingErSatt(annenStilling)).to.equal(true);
-        expect(sisteStillingErSatt(ingenYrkesbakgrunn)).to.equal(true);
-        expect(sisteStillingErSatt(sisteStillingMock)).to.equal(true);
+        expect(sisteStillingErSatt(tomStilling)).toEqual(false);
+        expect(sisteStillingErSatt(undefined)).toEqual(false);
+        expect(sisteStillingErSatt(annenStilling)).toEqual(true);
+        expect(sisteStillingErSatt(ingenYrkesbakgrunn)).toEqual(true);
+        expect(sisteStillingErSatt(sisteStillingMock)).toEqual(true);
     });
 
 });
