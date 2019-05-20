f089c5cb1f63df325860ba089941bf46b8149d20
Joselito Poblete <joselito.poblete@gmail.com>
2019-05-20 14:36:46 +0200

    FO-2088 Mock riktig

--- src/mocks/autexpiration-mock.ts
+++ src/mocks/autexpiration-mock.ts
@@ -1,11 +1,12 @@
 import * as moment from 'moment';
 export const tisekunder = 10;
 const femMinutter = 300;
+const enTime = 3600;
 
 const autexpirationMock = {
     remainingSeconds: 1121,
     expirationTime:
-        moment(new Date(), '').add(femMinutter + tisekunder,  'seconds'),
+        moment(new Date(), '').add(enTime + femMinutter + tisekunder,  'seconds'),
 };
 
 export default autexpirationMock;
