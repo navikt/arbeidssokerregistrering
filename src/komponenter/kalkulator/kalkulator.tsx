753af28af8aa26ab80c537eae599b017bcb6a8b1
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-07-02 08:28:42 +0200

    [FO-2236] - legger til utregning

--- src/komponenter/kalkulator/kalkulator.tsx
+++ src/komponenter/kalkulator/kalkulator.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Input } from 'nav-frontend-skjema';
 
+function createDate (date) {
+  return new Date(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+}
 class Kalkulator extends React.Component<{}, { sisteArbeidsdag: string }> {
   constructor(props) {
     super(props);
@@ -10,12 +13,21 @@ class Kalkulator extends React.Component<{}, { sisteArbeidsdag: string }> {
   
   regnUtPeriode (e) {
     e.preventDefault();
-    const dato = e.target.value;
-    const fristStart = new Date(dato);
-    const fristSlutt = new Date(dato);
+    let melding = '';
+    const now = new Date();
+    const iDag = createDate(now);
+    const dato = new Date(e.target.value);
+    const fristStart = createDate(dato);
+    const fristSlutt = createDate(dato);
     fristStart.setDate(fristStart.getDate() - 7);
     fristSlutt.setDate(fristSlutt.getDate() - 1);
-    this.setState({ sisteArbeidsdag: `${fristStart} - ${fristSlutt}`});
+    const dagerTilFrist = (fristSlutt.getTime() - iDag.getTime())/86400000;
+    if (dagerTilFrist < 7) {
+      melding = `Du bør søke i dag ${dagerTilFrist}`;
+    } else {
+      melding = `Du bør søke i perioden ${dagerTilFrist} ${fristStart} - ${fristSlutt}`;
+    }
+    this.setState({ sisteArbeidsdag: `${melding}`});
   }
 
   render () {
