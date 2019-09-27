8ccd763de8bcee4c6fe8f8375f1d659e85577fb0
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-09-27 12:01:18 +0200

    [KAIZEN-0] - legger inn velger for servicegruppe og formidlingsgruppe på demo

--- src/mocks/registreringstatus-mock.ts
+++ src/mocks/registreringstatus-mock.ts
@@ -1,4 +1,4 @@
-import { RegistreringType } from '../ducks/registreringstatus';
+import { RegistreringType, Servicegruppe, Formidlingsgruppe } from '../ducks/registreringstatus';
 import { Data as RegStatusData } from '../ducks/registreringstatus';
 import { JSONObject } from 'yet-another-fetch-mock/dist/types/types';
 import moment from 'moment';
@@ -11,5 +11,7 @@ export default {
     underOppfolging: false,
     jobbetSeksAvTolvSisteManeder: false,
     maksDato: moment(new Date(), 'DD.MM.YYYY').add(13, 'week'),
-    registreringType: RegistreringType.ORDINAER_REGISTRERING
+    registreringType: RegistreringType.ORDINAER_REGISTRERING,
+    servicegruppe: Servicegruppe.IVURD,
+    formidlingsgruppe: Formidlingsgruppe.ARBS
 } as RegStatusData&JSONObject&MaksDatoMomentType;
