bc856d862d7cf9746191df96d4be0de2dff42d92
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-11-12 06:58:28 +0100

    [KAIZEN-0] - refactorerer plaster til egen fil

--- src/sider/allerede-registrert/allerede-registrert.tsx
+++ src/sider/allerede-registrert/allerede-registrert.tsx
@@ -8,8 +8,7 @@ import Banner from '../../komponenter/banner/banner';
 import { frontendLogger } from '../../metrikker/metrics-utils';
 import { AppState } from '../../reducer';
 import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
-import { Fieldset, Radio } from 'nav-frontend-skjema';
-import { Knapp } from 'nav-frontend-knapper';
+import IARBSPlaster from './iarbs-plaster'
 import hash from '../../utils/hash'
 
 import './allerede-registrert.less';
@@ -40,27 +39,6 @@ class AlleredeRegistrert extends React.Component<Props> {
         frontendLogger('registrering.allerede-registrert.click.dialog', { formidlingsgruppeTag: formidlingsgruppe, servicegruppeTag: servicegruppe, geografiskTilknytningTag: geografiskTilknytning}, {});
     }
 
-    handleClickKontaktMeg (event) {
-        const kvittering = document.getElementById('kontaktMegKvittering')
-        const melding = document.getElementById('kontaktMegMeldingWrapper')
-        if (melding && kvittering) {
-            melding.className = 'hidden'
-            kvittering.className = 'blokk-s'
-        }
-    }
-
-    handleClickContact (event) {
-        const melding = document.getElementById('kontaktMegMelding')
-        const knapp = document.getElementById('kontaktMegKnapp')
-        if (melding && knapp && event.target.id === 'kontaktMegAnnet') {
-            melding.className = 'blokk-s'
-            knapp.className = 'hidden'
-        }
-        if (melding && knapp && event.target.id === 'kontaktMegDagpenger') {
-            melding.className = 'hidden'
-            knapp.className = 'knapp'
-        }
-    }
     render() {
         const messages = this.props.intl.messages;
         const formidlingsgruppe = this.props.state.registreringStatus.data.formidlingsgruppe;
@@ -98,30 +76,7 @@ class AlleredeRegistrert extends React.Component<Props> {
                             </AlertStripeAdvarsel>
                         </Column>
                     </Row> : null}
-                    {newPlaster ? <Row className="">
-                        <Column xs="12" sm="8" className="allerede-registrert__boks">
-                            <div className="allerede-registrert__boks-innhold venstrejustert">
-                                <Normaltekst className="blokk-s">Vi ser at du prøver å registrere deg som arbeidssøker.</Normaltekst>
-                                <Normaltekst className="blokk-s"><strong>Hvis du prøver å registrere deg fordi du ønsker å søke dagpenger, må du ta kontakt med NAV.</strong></Normaltekst>
-                                <div className="blokk-s" id="kontaktMegMeldingWrapper">
-                                    <Fieldset legend="">
-                                        <Radio label={'Ja, jeg skal søke dagpenger og vil bli kontaktet av en veileder'} name="kontaktmeg" id="kontaktMegDagpenger" onChange={this.handleClickContact} />
-                                        <Radio label={'Jeg ønsker å snakke med en veileder uavhengig av dagpenger'} id="kontaktMegAnnet" onChange={this.handleClickContact} name="kontaktmeg" />
-                                    </Fieldset>
-                                    <Normaltekst className="hidden" id="kontaktMegMelding">
-                                        Hvis du ønsker å snakke om noe annet enn dagpenger, må du ringe 55 55 33 33.
-                                    </Normaltekst>
-                                    <div className="midtjustert">
-                                        <Knapp className="hidden" id="kontaktMegKnapp" onClick={this.handleClickKontaktMeg}>Send inn henvendelsen</Knapp>
-                                    </div>
-                                </div>
-                                <div className="hidden" id="kontaktMegKvittering">
-                                    <Normaltekst>Din henvendelse er mottatt</Normaltekst>
-                                    <Normaltekst>Forventet svartid på denne henvendelsen er 2 arbeidsdager.</Normaltekst>
-                                </div>
-                            </div>
-                        </Column>
-                    </Row> : null}
+                    {newPlaster ? <IARBSPlaster /> : null}
                     <Row className="">
                         <Column xs="12" sm="8" className="allerede-registrert__boks">
                             <div className="allerede-registrert__boks-innhold">
