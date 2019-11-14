579a0f0068527e8ab72f5bc4abda10cce1bbf9d6
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-11-14 08:35:11 +0100

    [KAIZEN-0] - legger til manglende fil

---
+++ src/metrikker/amplitude-utils.ts
@@ -0,0 +1,21 @@
+// tslint:disable align no-any
+import amplitude from 'amplitude-js';
+const API_KEY = ''
+const config = {
+  apiEndpoint: 'amplitude.nav.no/collect',
+  saveEvents: true,
+  includeUtm: true,
+  includeReferrer: true,
+  trackingOptions: {
+     city: false,
+     ip_address: false, 
+  }
+}
+
+amplitude.getInstance().init(API_KEY, null, config)
+
+export type AmplitudeLogger = (name: string, values?: object) => void;
+
+export function amplitudeLogger(name: string, values?: object) {
+  amplitude.logEvent(name, values)
+}
