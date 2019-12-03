cc6a58d74cf2a14052fa2fba4918bab6636102fb
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-12-03 13:51:02 +0100

    [KAIZEN-0] - setter toggle til false

--- src/mocks/feature-toggles-mock.ts
+++ src/mocks/feature-toggles-mock.ts
@@ -1,5 +1,5 @@
 export const featureTogglesMock = {
-    'arbeidssokerregistrering.nyttaar': true,
+    'arbeidssokerregistrering.nyttaar': false,
     'arbeidssokerregistrering.nedetid': false,
     'arbeidssokerregistrering.kontaktmeg.kontor-030102': true,
     'arbeidssokerregistrering.kunngjoring': false
