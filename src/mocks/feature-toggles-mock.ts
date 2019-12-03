d72daa57a43a6fb4fe10aadd3a569c11a78fb48a
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-12-03 08:02:52 +0100

    [KAIZEN-0] - tekstfiks og refaktorering 

--- src/mocks/feature-toggles-mock.ts
+++ src/mocks/feature-toggles-mock.ts
@@ -1,5 +1,5 @@
 export const featureTogglesMock = {
     'arbeidssokerregistrering.nedetid': false,
     'arbeidssokerregistrering.kontaktmeg.kontor-030102': true,
-    'arbeidssokerregistrering.kunngjoring': false
+    'arbeidssokerregistrering.kungjoring': false
 };
