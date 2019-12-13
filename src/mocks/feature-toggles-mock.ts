682dfbcc5dd4653bb7fe8cb352bf8006db0ec985
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-12-13 10:53:14 +0100

    [KAIZEN-0] - ny side for brukere som har SPERRET

--- src/mocks/feature-toggles-mock.ts
+++ src/mocks/feature-toggles-mock.ts
@@ -2,5 +2,6 @@ export const featureTogglesMock = {
     'arbeidssokerregistrering.nedetid.nyttaar': false,
     'arbeidssokerregistrering.nedetid': false,
     'arbeidssokerregistrering.kontaktmeg.kontor-030102': true,
-    'arbeidssokerregistrering.kunngjoring': false
+    'arbeidssokerregistrering.kunngjoring': false,
+    'arbeidssokerregistrering.sperret.ny-versjon': true
 };
---
+++ src/sider/info-for-ikke-arbeidssoker-uten-oppfolging/original-melding.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { Normaltekst } from 'nav-frontend-typografi';
+import Veilederpanel from 'nav-frontend-veilederpanel';
+import utropstegnSvg from '../fullfor/utropstegn.svg';
+
+const melding = () => {
+  return (
+    <div className="info-for-ikke-arbeidssoker">
+      <Veilederpanel
+                type="plakat"
+                svg={<img
+                    src={ utropstegnSvg }
+                    alt="Informasjon"
+                    className="nav-veilederpanel__illustrasjon"
+                />}
+                kompakt={true}
+            >
+                <Normaltekst>
+                    <FormattedMessage id="info-for-ikke-arbeidssoker-uten-oppfolging-innhold"/>
+                </Normaltekst>
+            </Veilederpanel>
+    </div>
+  )
+}
+
+export default melding
