b38b5d8a2dc39ef8a8edd9bbf40d018023f6cc37
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-29 18:49:56 +0100

    Legger inn logging av flere feilmeldinger (patch)

--- src/komponenter/feilmelding/feilmelding-generell.tsx
+++ src/komponenter/feilmelding/feilmelding-generell.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import { FormattedHTMLMessage } from 'react-intl';
 import Feilmelding from './feilmelding';
 import { Normaltekst } from 'nav-frontend-typografi';
+import { uniLogger } from '../../metrikker/uni-logger';
 
 function FeilmeldingGenerell({ tekstId }: { tekstId?: string }) {
     const id = tekstId ? tekstId : 'feilmelding-generell';
+    uniLogger('arbeidssokerregistrering.error', { feilType: id })
     return (
         <Feilmelding>
             <Normaltekst>
