d543855f9c5f9eef40a5b0ea9c62e7c06cd8eb58
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-03-06 13:59:08 +0100

    Krever bekreftelse for å gå videre (patch)

--- src/komponenter/iarbs-registrering-modal/iarbs-registrering-modal.tsx
+++ src/komponenter/iarbs-registrering-modal/iarbs-registrering-modal.tsx
@@ -23,9 +23,20 @@ interface StateProps {
     registreringType?: RegistreringType;
 }
 
+interface LocaleState {
+    isNotConfirmed: boolean;
+}
+
 type AllProps = OwnProps & StateProps;
 
 class AvbrytModal extends React.Component<AllProps> {
+    constructor (props: AllProps) {
+        super(props);
+
+        this.state = {
+            isNotConfirmed: true
+        }
+    }
 
     handleRegistreringKnappClicked = () => {
         uniLogger('registrering.iarbs.registrering.modal.registrer');
@@ -37,7 +48,17 @@ class AvbrytModal extends React.Component<AllProps> {
         this.props.onRequestClose()
     }
 
+    setConfirmed = (value: boolean) => {
+        this.setState({
+            isNotConfirmed: value
+        })
+    }
+
     render() {
+        const toggleConfirm = event => {
+            this.setConfirmed(!event.target.checked)
+        }
+
         return (
             <NavFrontendModal
                 isOpen={this.props.isOpen}
@@ -61,13 +82,11 @@ class AvbrytModal extends React.Component<AllProps> {
 
                     <Normaltekst className="blokk-m">
                         Å registrere deg som arbeidssøker betyr at du ikke lenger vil være registrert som sykmeldt hos NAV.<br/>
-                        Hvis du mottar eller har rett til sykepenger risikerer du å miste retten til disse da du ikke kan motta to ytelser samtidig.
-                        <p>
-                            <Checkbox label={'Jeg bekrefter at jeg vil registrere meg som arbeidssøker'} />
-                        </p>
+                        Hvis du mottar eller har rett til sykepenger risikerer du å miste retten til disse da du ikke kan motta to ytelser samtidig.                        
                     </Normaltekst>
+                    <Checkbox label={'Jeg bekrefter at jeg vil registrere meg som arbeidssøker'} onClick={toggleConfirm} />
                     <div className="avbryt-modal">
-                        <Knapp className="avbryt-modal__knapp blokk-s" onClick={this.handleRegistreringKnappClicked}>
+                        <Knapp className="avbryt-modal__knapp blokk-s" id="confirmKnapp" onClick={this.handleRegistreringKnappClicked} disabled={this.state['isNotConfirmed']}>
                             Ja, jeg vil registrere meg som arbeidssøker
                         </Knapp>
                         <Knapp className="avbryt-modal__knapp" onClick={this.handleRegistreringAvbrytKnappClicked}>
