702c33b265c73cd569b533e8f0a1c1ec3451d384
Johan Rusvik <johan.rusvik@nav.no>
2020-03-06 14:08:38 +0100

    Oppdaterer metrikk-id

--- src/utils/antall-dager-til-maksdato.ts
+++ src/utils/antall-dager-til-maksdato.ts
@@ -5,7 +5,7 @@ export const antallDagerTilMaksdato = (naa: Date, maksdato: Date): number => {
     const diff = maksdato.getTime() - naa.getTime();
     const antallDager = Math.floor(diff / 1000 / 60 / 60 / 24);
 
-    uniLogger('arbeidssøkerregistrering.antall-dager-sykmeldt.data', {
+    uniLogger('arbeidssøkerregistrering.antall-dager-til-maksdato.data', {
         maksdato, 
         antallDagerTilMaksdato: antallDager,
     });
