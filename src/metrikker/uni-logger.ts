98c369056a462067ed0ef1ca41d23b8af58608c9
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-03-18 16:20:15 +0100

    Pauser amplitude (patch)

--- src/metrikker/uni-logger.ts
+++ src/metrikker/uni-logger.ts
@@ -1,17 +1,19 @@
 import { name as appname, version as appversion } from '../../package.json';
-import { amplitudeLogger } from './amplitude-utils';
+// import { amplitudeLogger } from './amplitude-utils';
 // import { amplitudeLogger, getDeviceId } from './amplitude-utils';
 // import { frontendLogger } from './metrics-utils';
 
 // const deviceId = getDeviceId();
-
+// eslint-disable-next-line
 const prefix = {
   appname,
   appversion
 };
 
+
 export function uniLogger(name: string, values?: object) {
-  const data = values || {};
-  amplitudeLogger(name, {...prefix, ...data});
+  // const data = values || {};
+  // amplitudeLogger(name, {...prefix, ...data});
   // frontendLogger(name, { deviceId }, {...prefix, ...data});
+  return true
 }
