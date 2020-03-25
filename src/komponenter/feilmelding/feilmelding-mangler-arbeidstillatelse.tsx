a822ebb61651dfe7cce38ce871520a1a42b4f946
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-03-25 12:35:52 +0100

    Oppdaterer tekst for manglende oppholdstillatelse (patch)

--- src/komponenter/feilmelding/feilmelding-mangler-arbeidstillatelse.tsx
+++ src/komponenter/feilmelding/feilmelding-mangler-arbeidstillatelse.tsx
@@ -1,34 +1,21 @@
 import * as React from 'react';
 import { Normaltekst } from 'nav-frontend-typografi';
-import { FormattedMessage } from 'react-intl';
-import LenkeMedChevron from '../../komponenter/lenke-med-chevron/lenke-med-chevron';
 import { InjectedIntlProps } from 'react-intl';
 import Feilmelding from './feilmelding';
 import utropstegnSvg from '../../sider/fullfor/utropstegn.svg';
-import { erIFSS } from '../../utils/fss-utils';
-import { lagAktivitetsplanUrl } from '../../utils/url-utils';
 import './feilmelding-mangler-arbeidstillatelse.less';
 
 const FeilmeldingManglerArbeidstillatelse: React.SFC<InjectedIntlProps> = (props: InjectedIntlProps) => {
 
-    let innholdId = 'feilmelding-mangler-arbeidstillatelse-innhold';
-    let lenkeTekstId = 'feilmelding-mangler-arbeidstillatelse-lenke-tekst';
-    let lenkeUrl = props.intl.messages['finn-ditt-nav-kontor-lenke-url'];
-
-    if (erIFSS()) {
-        innholdId += '-fss';
-        lenkeTekstId += '-fss';
-        lenkeUrl = lagAktivitetsplanUrl();
-    }
-
     return (
         <Feilmelding bilde={utropstegnSvg} className="feilmelding-mangler-arbeidstillatelse">
             <Normaltekst className="blokk-s">
-                <FormattedMessage id={innholdId}/>
+                Vi har ikke mulighet i våre systemer til å sjekke om du har en godkjent oppholdstillatelse.
+                Du kan derfor ikke registrere deg som arbeidssøker nå.
+                <p>
+                    Ring oss på 55 55 33 33 så hjelper vi deg videre.
+                </p>
             </Normaltekst>
-            <LenkeMedChevron path={lenkeUrl}>
-                <FormattedMessage id={lenkeTekstId}/>
-            </LenkeMedChevron>
         </Feilmelding>
     );
 };
