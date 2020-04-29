1908d5546c89655f221d5c484bdd31add6d4caa1
Castigere <even.haasted@gmail.com>
2020-04-29 08:53:04 +0200

    Import og transpiling av tsx react komponenter og less virker i cypress
    Huskeliste for neste gang:
    
    * Webpack preprocessor mÃ¥settes opp med
     - less-loader
     - babel-loader for .js med @babel/preset-react
     - ts-loader for ts og tsx uten typechecking. (transpileOnly: true)
    * Nyeste versjon av ts-loader, versjonen inkludert i
    add-typescript-to-cypress er for gammel
    * Endre jsx -parameter til "react" i tslint i root

--- cypress/plugins/cy-ts-preprocessor.js
+++ cypress/plugins/cy-ts-preprocessor.js
@@ -2,22 +2,57 @@ const wp = require('@cypress/webpack-preprocessor')
 
 const webpackOptions = {
   resolve: {
-    extensions: ['.ts', '.js', '.tsx']
+    extensions: ['.ts', '.js', '.tsx', 'jsx']
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         exclude: [/node_modules/],
         use: [
           {
-            loader: 'ts-loader'
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true
+            }
           }
         ]
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                strictMath: true,
+              },
+            },
+          },
+        ],
       }
     ]
-  }
-}
+  },
+},
+
 
 const options = {
   webpackOptions
---
+++ cypress/specs/arbeidssokerregistrering/react/komponenter-ekspanderbartinfo-spec.js
@@ -0,0 +1,29 @@
+/*tslint:disable*/
+import React from "react";
+import * as enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import EkspanderbartInfo from "../../../../src/komponenter/ekspanderbartinfo/ekspanderbartInfo";
+import { shallowwithIntl } from "../../../../src/test/test-utils";
+
+enzyme.configure({ adapter: new Adapter() });
+
+describe("<EkspanderbartInfo />", () => {
+  it("Skal default ikke vise innhold", () => {
+    const wrapper = shallowwithIntl(
+      <EkspanderbartInfo tittelId="fullfor-les-mer">
+        <span className="Dummy" />
+      </EkspanderbartInfo>
+    );
+    expect(wrapper.find(".Dummy")).to.have.length(0);
+  });
+
+  it("Skal vise innhold når knapp klikkes", () => {
+    const wrapper = shallowwithIntl(
+      <EkspanderbartInfo tittelId="fullfor-les-mer">
+        <span className="Dummy" />
+      </EkspanderbartInfo>
+    );
+    wrapper.find(".knapp-reset").simulate("click");
+    expect(wrapper.find(".Dummy")).to.have.length(1);
+  });
+});
