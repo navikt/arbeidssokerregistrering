0af43fd944f9017a24eb02ff35d3197ec0268ca1
Johan Rusvik <johan.rusvik@nav.no>
2020-06-05 10:35:16 +0200

    Legger til informasjon om MinID Passport

--- src/komponenter/initialdata/info-elektronisk-id.tsx
+++ src/komponenter/initialdata/info-elektronisk-id.tsx
@@ -5,24 +5,28 @@ import { amplitudeLogger } from '../../metrikker/amplitude-utils';
 import './info-kort.less';
 import LenkeMedChevron from '../lenke-med-chevron/lenke-med-chevron';
 import bankidSvg from './bankid.svg';
+import Lenke from 'nav-frontend-lenker';
 
 const InfoElektroniskId = () => {
     const { location } = window;
-    const lenke = 'https://eid.difi.no/nb/bankid';
-    const getRetningFraNAV = () => /nav.no/.test(lenke) ? 'inn' : 'ut';
-    const handleUtgangsLenkeKlikk = () => {
+    const lenkeBankID = 'https://eid.difi.no/nb/bankid';
+    const lenkePassportIDEng = 'https://eid.difi.no/en/minid/passport';
+    const lenkePassportIDNor = 'https://www.nav.no/no/person/innhold-til-person-forside/nyttig-a-vite/minid-passport--mangler-du-elektronisk-id-til-navs-digitale-tjenester';
+
+    const getRetningFraNAV = (lenke) => /nav.no/.test(lenke) ? 'inn' : 'ut';
+    const handleUtgangsLenkeKlikk = (lenke) => {
         const data = {
             side: location.pathname,
-            retning: getRetningFraNAV(),
+            retning: getRetningFraNAV(lenke),
             lenke
         };
         amplitudeLogger('lenke', data);
         return true;
     };
-    const Lenke = () => {
+    const LenkeBankID = ({classes}) => {
         return (
-            <LenkeMedChevron path={lenke} target="_blank" onClick={ handleUtgangsLenkeKlikk }>
-                <FormattedMessage id="overskrift-elektronisk-hjelp-stepup"/>
+            <LenkeMedChevron path={lenkeBankID} target="_blank" onClick={ () => handleUtgangsLenkeKlikk(lenkeBankID) } className={classes}>
+                <FormattedMessage id="overskrift-elektronisk-hjelp-stepup" />
             </LenkeMedChevron>
         );
     };
@@ -33,7 +37,29 @@ const InfoElektroniskId = () => {
             <Normaltekst className="blokk-s">
                 <FormattedHTMLMessage id="beskrivelse-elektronisk-hjelp-stepup"/>
             </Normaltekst>
-            <Lenke />
+            <LenkeBankID classes="blokk-s" />
+            <Undertittel>MinID Passport</Undertittel>
+            <Normaltekst className="blokk-s">
+                MinID Passport is an electronic ID solution which can be used to log into nav.no. MinID Passport&nbsp;
+                <Lenke
+                    href={lenkePassportIDEng}
+                    target="_blank"
+                    onClick={() => handleUtgangsLenkeKlikk(lenkePassportIDEng)}
+                >
+                    uses passport verification
+                </Lenke>
+                .
+            </Normaltekst>
+            <Normaltekst>MinID Passport er en elektronisk ID som kan brukes for å logge på nav.no. MinID Passport&nbsp;
+                <Lenke
+                    href={lenkePassportIDNor}
+                    target="_blank"
+                    onClick={() => handleUtgangsLenkeKlikk(lenkePassportIDNor)}
+                >
+                    bruker pass som bekreftelse
+                </Lenke>
+                .
+            </Normaltekst>
         </div>
     );
 };
