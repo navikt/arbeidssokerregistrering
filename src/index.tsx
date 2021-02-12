edef43894a76586356297a140d9c033a42ef1eda
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-02-12 12:58:20 +0100

    Legger til Sentry
    Kjører foreløpig kun i det vi regner som «produksjon» for å unngå potensiell støy

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import * as ReactDOM from "react-dom";
 import "idempotent-babel-polyfill";
 import "react-app-polyfill/ie11";
 import Modal from "react-modal";
+import * as Sentry from "@sentry/react";
 import moment from "moment";
 import "moment/locale/nb";
 import { erIFSS } from "./utils/fss-utils";
+import { erProduksjon } from "./utils/url-utils";
 import App from "./app";
 import AppFss from "./app-fss";
 import "./index.less";
@@ -35,6 +37,13 @@ if (process.env.REACT_APP_MOCK) {
   /* ************************************************************************************** */
 
   require("./mocks/mocks");
+}
+
+if (erProduksjon()) {
+  Sentry.init({
+    dsn: "https://52908dd3ce2a4fde8bd57bc1cd03651c@sentry.gc.nav.no/66",
+    environment: erProduksjon() ? "production" : "test",
+  });
 }
 
 ReactDOM.render(erIFSS() ? <AppFss /> : <App />, document.getElementById("root") as HTMLElement);
