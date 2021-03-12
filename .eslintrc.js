06793055b196ecf298afce6b9bcf9c02669ed578
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2021-03-12 13:28:42 +0100

    Slå av TypeScript warnings ved CI/GHA

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,25 @@
-module.exports = {
-  extends: ["react-app", "eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  rules: {
-    "@typescript-eslint/ban-types": "warn",
-    "@typescript-eslint/ban-ts-comment": "warn",
-    "@typescript-eslint/no-empty-interface": "warn",
-    "@typescript-eslint/no-var-requires": "warn",
-    "@typescript-eslint/no-inferrable-types": "warn",
-    "no-extra-boolean-cast": "warn",
-    "react-hooks/exhaustive-deps": "warn",
-  },
-};
+process.env.CI
+  ? (module.exports = {
+      extends: ["react-app"],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      rules: {
+        "import/no-anonymous-default-export": "off",
+      },
+    })
+  : (module.exports = {
+      extends: ["react-app", "eslint:recommended", "plugin:@typescript-eslint/recommended"],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      rules: {
+        "@typescript-eslint/ban-types": "warn",
+        "@typescript-eslint/ban-ts-comment": "warn",
+        "@typescript-eslint/no-empty-interface": "warn",
+        "@typescript-eslint/no-var-requires": "warn",
+        "@typescript-eslint/no-inferrable-types": "warn",
+        "no-extra-boolean-cast": "warn",
+        "react-hooks/exhaustive-deps": "warn",
+      },
+    });
