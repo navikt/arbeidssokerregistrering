720e9f8d78ed362f1b3b708ead228e9f42be0827
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-05-06 11:15:51 +0200

    e gjenforent eslint-konfigurasjon

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,13 @@
-process.env.CI
-  ? (module.exports = {
-      extends: ["react-app"],
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-      rules: {
-        "import/no-anonymous-default-export": "off",
-      },
-    })
-  : (module.exports = {
-      extends: ["react-app", "eslint:recommended", "plugin:@typescript-eslint/recommended"],
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-      rules: {
-        "@typescript-eslint/ban-types": "warn",
-        "@typescript-eslint/ban-ts-comment": "warn",
-        "@typescript-eslint/no-empty-interface": "warn",
-        "@typescript-eslint/no-var-requires": "warn",
-        "@typescript-eslint/no-inferrable-types": "warn",
-        "no-extra-boolean-cast": "warn",
-        "react-hooks/exhaustive-deps": "warn",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "import/no-anonymous-default-export": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-      },
-    });
+module.exports = {
+  extends: ["react-app", "eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parserOptions: {
+    project: "./tsconfig.json",
+    cache: false,
+  },
+
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "import/no-anonymous-default-export": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
