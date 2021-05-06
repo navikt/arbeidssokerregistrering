865826a831160c8a91a7913d742f89f09ac77856
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-05-06 10:17:46 +0200

    F endrer metrikknavn til å følge aktivitet og visning navngivning

--- src/sider/allerede-registrert/allerede-registrert.tsx
+++ src/sider/allerede-registrert/allerede-registrert.tsx
@@ -35,7 +35,8 @@ class AlleredeRegistrert extends React.Component<Props> {
     const servicegruppe = event.currentTarget.dataset.servicegruppe;
     const geografiskTilknytning = event.currentTarget.dataset.geografisktilknytning;
     const rettighetsgruppe = event.currentTarget.dataset.rettighetsgruppe;
-    uniLogger("registrering.allerede-registrert.click.veienTilArbeid", {
+    uniLogger("registrering.aktivitet", {
+      aktivitet: "Går til veien til arbeid fra allerede registrert siden",
       formidlingsgruppe,
       servicegruppe,
       geografiskTilknytning,
@@ -48,7 +49,8 @@ class AlleredeRegistrert extends React.Component<Props> {
     const servicegruppe = event.currentTarget.dataset.servicegruppe;
     const geografiskTilknytning = event.currentTarget.dataset.geografisktilknytning;
     const rettighetsgruppe = event.currentTarget.dataset.rettighetsgruppe;
-    uniLogger("registrering.allerede-registrert.click.dialog", {
+    uniLogger("registrering.aktivitet", {
+      aktivitet: "Går til dialogen fra allerede registrert siden",
       formidlingsgruppe,
       servicegruppe,
       geografiskTilknytning,
@@ -65,7 +67,8 @@ class AlleredeRegistrert extends React.Component<Props> {
     const geografiskTilknytning = this.props.state.registreringStatus.data.geografiskTilknytning || "INGEN_VERDI";
     const rettighetsgruppe = this.props.state.registreringStatus.data.rettighetsgruppe;
     const isIARBS = formidlingsgruppeOrIngenVerdi === "IARBS";
-    uniLogger("registrering.allerede-registrert.sidevisning", {
+    uniLogger("registrering.visning", {
+      viser: "Viser allerede registrert siden",
       formidlingsgruppe,
       servicegruppe,
       geografiskTilknytning,
