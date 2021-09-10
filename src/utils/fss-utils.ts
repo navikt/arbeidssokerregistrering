0e88fc25d84884cc17428099d19abbaec44e82ad
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2021-09-10 08:48:04 +0200

    r fjerner ting som ikke er bruk

--- src/utils/fss-utils.ts
+++ src/utils/fss-utils.ts
@@ -1,11 +1,7 @@
 import { hentBrukerIKontekst, oppdaterAktivBruker } from "../ducks/api";
-import { hentQueryParameter, lagAktivitetsplanUrl } from "./url-utils";
+import { hentQueryParameter } from "./url-utils";
 import mockedBrukerFnr from "../mocks/bruker-fnr";
 import mockedVeilederEnhetId from "../mocks/veileder-enhet-id";
-
-interface PersonsokEvent extends Event {
-  fodselsnummer: string;
-}
 
 const EXPIRES_AFTER = 10000; // ms
 const BRUKER_FNR_TAG = "FSS_KONTEKST_BRUKER_FNR";
